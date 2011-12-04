0ae010b3bfcf5942b7c55ed5a2e809505312c782
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2011-12-05 01:27:08 +0800

    Added type register

--- src/typed_clojure/core.clj
+++ src/typed_clojure/core.clj
@@ -9,30 +9,32 @@
   (type-predicate [this]))
 
 (defn type? [t]
-  (contains? (ancestors (class t)) Type))
+  (isa? (class t) Type))
 
 (def Type ::Type)
 
-(defmacro def-type [name & body]
+(defmacro def-type-constructor [name & body]
   `(do
      (c/defconstrainedtype ~name
                            ~@body)
-     (derive ~name Type)))
+     (derive ~name Type))) ;; TODO Should this be the global hierarchy?
 
 ;; Union Type
 
-(def-type Union 
-          [elems]
-          [(every? type? elems)])
+(def-type-constructor 
+  Union 
+  [elems]
+  [(every? type? elems)])
 
 (defn Un [& types]
   (->Union types))
 
 ;; Numeric Types
 
-(def-type Base 
-          [name pred]
-          [(symbol? name)])
+(def-type-constructor 
+  Base 
+  [name pred]
+  [(symbol? name)])
 
 (def Zero (->Base 'Zero #(= 0 %1)))
 (def One  (->Base 'One #(= 1 %1)))
@@ -43,26 +45,66 @@
 
 ;; Functions
 
-(def-type Function 
-          [dom rng]
-          [(every? type? dom)
-           (type? rng)])
+(def-type-constructor 
+  Function 
+  [dom rng]
+  [(every? type? dom)
+   (type? rng)])
+
+;; Type ann database
+
+(def type-anns (atom {}))
+
+(defn add-type-ann! [id type]
+  (swap! type-anns assoc id type))
+
+(defn get-type-ann [id]
+  (if-let [t (@type-anns id)]
+    t
+    (throw (Exception. (str "No type annotation for " id)))))
 
 ;; Primitives
 
+(defn register-def-type-ann [def-form]
+  (let [[_ name type] def-form]
+    (add-type-ann! `~name type)))
+
+;; Languages as Libraries, Sam TH
+(defn type-check 
+  "Type check a form"
+  [form]
+  (let [anal (cljs/analyze {} form)]
+    (case (:op anal)
+      :var (get-type-ann `~form)
+      :def (register-def-type-ann))))
+
 (defmacro def-typed [name type & body]
-  (let [name ^{::type `~type} name]
-    `(def ~name
-       ~@body)))
+  (let [form `(def ~name ~@body)]
+    (type-check form)
+    `~form))
 
 (defmacro defn-typed [name args-dom rng & body]
   (let [args (-> (map first args-dom) vec)
         dom (map (comp deref resolve second) args-dom)
         rng (-> rng resolve deref)
-        type (->Function dom rng)
-        name ^{::type type} name]
-    `(defn ~name ~args
-       ~@body)))
+        type (->Function dom rng)]
+    `(def-typed ~name ~type
+       (fn-typed ~args-dom
+         ~@body))))
+
+;; TODO how do fn's store type information? in args?
+(defmacro fn-typed [name ann-formals & body]
+  (let [args (-> (map first ann-formals) vec)
+        dom (map (comp deref resolve second) ann-formals)
+        args (map #(with-meta %1 {::type %2}) args dom)
+        type (->Function dom (type-check `(do ~@body)))
+        form `(fn ~name ~args 
+                ~@body)]
+    (type-check form)
+    `~form))
+
+(fn-typed [[a -Integer]]
+  a)
 
 ;; Examples
 
