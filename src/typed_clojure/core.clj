4eed0e85daa5250d47a2f70d773001ed0bffefdf
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2011-12-06 18:47:56 +0800

    Added custom syntax for types. Type check seems to run successfully

--- src/typed_clojure/core.clj
+++ src/typed_clojure/core.clj
@@ -31,7 +31,8 @@
   [(every? type? elems)]
 
   Object
-  (toString [_] (str "(Un " (apply str (interpose " " elems)) ")")))
+  (toString [_] (str "(Un " (apply str (interpose " " elems)) ")"))
+  (equals [_ that] (= (.elems ^Union that) elems)))
 
 (defn Un [& types]
   (->Union types))
@@ -67,7 +68,9 @@
    (type? rng)]
   
   Object
-  (toString [_] (apply str (concat (apply str (interpose " " dom)) (str " -> " rng)))))
+  (toString [_] (apply str (concat (apply str (interpose " " dom)) (str " -> " rng))))
+  (equals [_ that] (and (= (.dom that) dom)
+                        (= (.rng that) rng))))
 
 (defn arity? [a]
   (instance? Arity a))
@@ -78,7 +81,8 @@
   [(every? arity? arrs)]
   
   Object
-  (toString [_] (apply str (interpose " , " (map str arrs)))))
+  (toString [_] (apply str (interpose " , " (map str arrs))))
+  (equals [_ that] (= (.arrs that) arrs)))
 
 ;; Type ann database
 
@@ -127,8 +131,10 @@
            (lookup-type (:form exp-obj)))
 
     :def (let [id (-> (:form exp-obj) second symbol-ns)
-               body (:init exp-obj)]
-           (type-check body (type-check id)))
+               body (:init exp-obj)
+               id-type (lookup-type id)
+               _ (assert id-type)]
+           (type-check body id-type))
 
     :if (let [then-type (type-check (:then exp-obj))
               else-type (type-check (:else exp-obj))]
@@ -137,27 +143,28 @@
           else-type)
 
     :fn (let [methods (:methods exp-obj)
-              arities (map (fn [{params :params children :children variadic :variadic}]
-                             (assert (not variadic) "Variadic functions not yet supported")
-                             (let [rng (map (comp ::type meta) params)
-                                   local-type-env (merge (map extend-local-type-env params rng))]
-                               (binding [*local-type-env* local-type-env]
-                                 ;; TODO type check each form in the body, while accumulating local type env ...
-                                 ;(type-check 
-                               )))
-                           methods)]
+              arities (doall
+                        (map (fn [{params :params variadic :variadic ret :ret}]
+                               (assert (not variadic) "Variadic functions not yet supported")
+                               (let [arg-types (doall (map (comp ::type meta) params)) ;; TODO bleh, store actual type in metadata
+                                     local-type-env (merge (map extend-local-type-env params arg-types))]
+                                 (binding [*local-type-env* local-type-env]
+                                   ;; TODO type check each form in the body, while accumulating local type env ...
+                                   (let [rng (type-check ret)
+                                         a (->Arity arg-types rng)]
+                                     a))))
+                             methods))]
           (->Function arities))
 
-    :invoke (let [id (-> exp-obj :f :form)
-                  id-type (type-check id)
-                  formals-type (.dom ^Function id-type)
+    :invoke (let [invoke-type (type-check (-> exp-obj :f))
+                  formals-type (.dom ^Function invoke-type)
                   actuals (-> exp-obj :args)
                   _ (when-not (= (count formals-type) (count actuals))
                       (type-error "Wrong number of arguments"))
                   _ (map type-check actuals formals-type)
                   rng (.rng ^Function formals-type)]
               rng)
-
+    (throw (Exception. (str exp-obj)))
     ))
 
 
@@ -173,9 +180,20 @@
        (type-error "Expected " expected-type " found " t))
      t)))
 
-(defmacro def. [name type & body]
-  (let [form `(def ^{:analyze {::type ~type}} ~name  ;;TODO need to quote ~name ?
-                ~@body)]
+
+;; Frontend
+
+(defn emit-type [t]
+  (cond
+    (type? t) t
+    (symbol? t) (-> t resolve deref)
+    (= 'Un (first t)) (apply Un (map emit-type (rest t)))   ;; Union
+    (= 'Fn (first t)) (->Function (map emit-type (rest t))) ;; Function
+    (= '-> (-> t butlast last)) (->Arity (map emit-type (-> t butlast butlast)) (emit-type (last t)))   ;; Arity
+    :else (assert (symbol? t) (str "Invalid type syntax " t))))
+
+(defmacro def-T [name & body]
+  (let [form `(def ~name ~@body)]
     (type-check (cljs/analyze {} form))
     form))
 
@@ -192,17 +210,17 @@
       (prepare-method body type)                ;; Single arity
       (map prepare-method body (.arrs type))))) ;; Multi arity
 
-(defmacro defn. [name & body]
+(defmacro defn-T [name & body]
   (let [type (lookup-type (symbol-ns name))
         _ (assert type)]
-    `(def. ~name
-       (fn. ~@(add-fn-arg-types body type)))))
+    `(def-T ~name
+       (fn-T ~@(add-fn-arg-types body type)))))
 
 (defn normalize-fn-arg-types
   "Convert fn body from (fn [[arg :- type]] ...) to (fn [^{:analyze {::type type} name] ..)"
   [body]
   (letfn [(prepare-param [[name _ type]]
-            (with-meta name {:analyze {::type type}}))
+            (with-meta name {:analyze {::type (emit-type type)}}))
           (prepare-arg-vector [v]
             (vec (map prepare-param v)))
           (prepare-method [[args & rest]]
@@ -211,23 +229,48 @@
       (prepare-method body)        ;; Single arity
       (map prepare-method body)))) ;; Multi arity
 
-(defmacro fn. [& body]
-  `(fn ~@(normalize-fn-arg-types body)))
+(defmacro fn-T [& body]
+  `(fn* ~@(normalize-fn-arg-types body))) ;; TODO should fn be a special form?
 
-(fn. [[a :- IntegerT]]
+;; TODO
+(defmacro let-T [bindings & body]
+  `(let ~@(normalize-fn-arg-types body)
+     ))
+
+(fn-T [[a :- IntegerT]]
   a)
 
-(defmacro T [id type]
-  (add-type-ann! (symbol-ns id) (eval type))
+(defmacro T [id _ type]
+  (add-type-ann! (symbol-ns id) (emit-type type))
   nil)
 
 ;; Examples
 
-(T asdf (->Function (list (->Arity (list IntegerT) IntegerT))))
-(defn. asdf
+;(let-T [[^{:T IntegerT} a 
+;        ^{:T Integer} b] [1 2]
+;       ^{:T FloatT} c 2.3]
+;  c)
+
+(T test-typed-def-fn :- (Fn (IntegerT -> IntegerT)))
+(def-T test-typed-def-fn
+  (fn-T 
+    ([[a :- IntegerT]] 1)))
+
+(T asdf :- (Fn (IntegerT -> IntegerT)))
+(defn-T asdf
   ([a] 1))
 
-(T a IntegerT)
-(def. a
-  1)
+(T a :- IntegerT)
+(def-T a 1)
 
+(T test-typed-def-fn :- (Fn (IntegerT -> IntegerT)))
+(def-T test-typed-def-fn
+  (fn-T 
+    ([[a :- IntegerT]] 1)))
+
+(T clojure.core/+ :- (Fn (IntegerT IntegerT -> IntegerT)
+                         (FloatT FloatT -> FloatT)))
+
+(T test-def-plus :- IntegerT)
+(def-T test-def-plus
+  (+ 1 2))
