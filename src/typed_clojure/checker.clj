d7aa43d7ca9b04dbfc6741888ad3772aaa3ae6b2
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2012-03-16 18:31:27 +0800

    Support map literal, static and instance methods, static fields

--- src/typed_clojure/checker.clj
+++ src/typed_clojure/checker.clj
@@ -1,5 +1,5 @@
 (ns typed-clojure.checker
-  (:import (clojure.lang Symbol Var IPersistentMap IPersistentCollection Keyword Namespace))
+  (:import (clojure.lang Symbol Var IPersistentMap IPersistentCollection Keyword Namespace Atom))
   (:use [analyze.core :only [analyze-path]])
   (:require [analyze.util :as util]
             [typed-clojure.flag :as flag]))
@@ -71,7 +71,6 @@
   "A protocol for specifying subtyping rules"
   (subtype [this sub] "A function to determine if sub is a subtype of this"))
 
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; # Types
 
@@ -110,7 +109,7 @@
 
 (defn arity [dom rng]
   (assert (every? #(or (= :& %)
-                       (satisfies? ITypedClojureType (class %)))
+                       (satisfies? ITypedClojureType %))
                   dom)
           dom)
   (assert (satisfies? ITypedClojureType (class rng)) rng)
@@ -166,19 +165,22 @@
 
   Class
   (subtype [this sub]
+    (assert (not (.isPrimitive this)))
     (if (instance? Union sub)
       (every? identity (map subtype (repeat this) (.types sub)))
       (isa? sub this)))
 
   IPersistentMap ;; Protocols
   (subtype [this sub]
-    (or (= this sub)
-        (satisfies? this sub)))
+    (if (instance? Union sub)
+      (every? identity (map subtype (repeat this) (.types sub)))
+      (satisfies? this sub)))
 
   nil
   (subtype [this sub]
-    (isa? sub this)))
+    (nil? sub)))
 
+;(+T subtype? (fun (arity [ITypedClojureType ITypedClojureType] Boolean)))
 (defn subtype? [type sub]
   (boolean (subtype type sub)))
 
@@ -231,23 +233,22 @@
 (+T fully-qualified (fun (arity [Symbol] (union nil Object))))
 ;clojure.core
 
-(+T clojure.core/resolve (fun (arity [Symbol] (union Var Class))
-                              (arity [IPersistentMap Symbol] (union Var Class))))
+(+T clojure.core/in-ns (fun (arity [Symbol] Namespace)))
+(+T clojure.core/resolve (fun (arity [Symbol] (union nil Var Class))
+                              (arity [IPersistentMap Symbol] (union nil Var Class))))
 (+T clojure.core/symbol (fun (arity [(union Symbol String)] Symbol)
                              (arity [String String] Symbol)))
 (+T clojure.core/str (fun (arity [:& Object] String)))
-(+T clojure.core/*ns* clojure.lang.Namespace)
-(+T clojure.core/atom (fun (arity [Object] clojure.lang.Atom)
-                           (arity [Object :& Object] clojure.lang.Atom)))
+(+T clojure.core/*ns* Namespace)
+(+T clojure.core/atom (fun (arity [Object :& Object] Atom)))
 (+T clojure.core/first (fun (arity [(union nil IPersistentCollection)] (union nil Object))))
 (+T clojure.core/rest (fun (arity [(union nil IPersistentCollection)] IPersistentCollection)))
 (+T clojure.core/next (fun (arity [(union nil IPersistentCollection)] (union nil IPersistentCollection))))
-(+T clojure.core/namespace (fun (arity [(union String Symbol Keyword)]
-                                       (union nil String))))
-(+T clojure.core/name (fun (arity [(union String Symbol Keyword)]
-                                  String)))
-(+T clojure.core/ns-name (fun (arity [Namespace]
-                                     Symbol)))
+(+T clojure.core/namespace (fun (arity [(union String Symbol Keyword)] (union nil String))))
+(+T clojure.core/name (fun (arity [(union String Symbol Keyword)] String)))
+(+T clojure.core/ns-name (fun (arity [Namespace] Symbol)))
+(+T clojure.core/refer (fun (arity [Symbol :& Object] nil)))
+(+T clojure.core/use (fun (arity [:& Object] nil)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; # Type Checker
@@ -265,8 +266,14 @@
   [{:keys [coll]}]
   (class coll))
 
+(defmethod type-check :map
+  [{:keys [keyvals]}]
+  (assert (every? identity (map type-check keyvals)))
+  clojure.lang.IPersistentMap)
+
 (defmethod type-check :vector
   [{:keys [args]}]
+  (assert (every? identity (map type-check args)))
   clojure.lang.IPersistentVector)
 
 (defmethod type-check :if
@@ -287,21 +294,46 @@
               (str arg-types " is not a subtype of " matched-arity))
       (.rng matched-arity))))
 
-(defn- method->fun [method]
-  (fun (arity (:parameter-types method)
-              (:return-type method))))
+;(+T resolve-class-symbol (fun (arity [Symbol] Class)))
+(defn- resolve-class-symbol 
+  {:post [#(class? %)]}
+  [sym]
+  (case sym
+    int Integer/TYPE
+    double Double/TYPE
+    (resolve sym)))
 
-(defmethod type-check :static-method
-  [{:keys [method args]}]
+;(+T method->fun (fun (arity [clojure.reflect.Method] Fun)))
+(defn- method->fun [method]
+  (fun (arity (map resolve-class-symbol (:parameter-types method))
+              (resolve-class-symbol (:return-type method)))))
+
+(defmethod type-check :static-field
+  [{:keys [field]}]
+  (assert (:type field))
+  (:type field))
+
+(defn- check-method [method args]
   (let [method-type (method->fun method)
         arg-types (map type-check args)
         matched-arity (matching-arity method-type args)]
-    (println args)
-    (println (.dom matched-arity) (map class (.dom matched-arity)))
-    (println arg-types)
     (assert (every? true? (map subtype? (.dom matched-arity) arg-types))
             (str arg-types " is not a subtype of " matched-arity))
     (.rng matched-arity)))
+
+(defmethod type-check :static-method
+  [{:keys [method args]}]
+  (check-method method args))
+
+(defmethod type-check :instance-method
+  [{:keys [method args]}]
+  (check-method method args))
+
+(defmethod type-check :try
+  [{:keys [try-expr catch-exprs finally-expr]}]
+  (do
+    (type-check finally-expr)
+    (apply union (map type-check (cons try-expr catch-exprs)))))
 
 (defmethod type-check :var
   [{:keys [var env]}]
@@ -333,6 +365,7 @@
   (let [fn-type (type-check fexpr)
         arg-types (doall (map type-check args))
         matched-arity (matching-arity fn-type args)]
+    (println "invoke" fexpr)
     (assert matched-arity (do
                            (util/print-expr expr :children :env :Expr-obj :ObjMethod-obj)
                            (println arg-types)
@@ -456,7 +489,7 @@
   (require nsym)
   (swap! flag/type-check-flag (constantly true))
   (let [analysis (analyze-path path nsym)]
-    (doseq [a (rest analysis)]
+    (doseq [a analysis]
       (type-check a)))
   (swap! flag/type-check-flag (constantly false)))
 
