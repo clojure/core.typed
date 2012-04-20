fb34986594c5f4c98d2f920f9c74fb4eba5d5e4e
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2012-04-21 01:54:24 +0800

    prepare 0.1-alpha1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject typed "0.1-SNAPSHOT"
+(defproject typed "0.1-alpha1"
   :description "Type Checker for Clojure, as a library"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [analyze "0.1.6"]
