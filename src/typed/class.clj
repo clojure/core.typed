4fc5e1b8ea31c1e545344aefb44bef585e43af17
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2012-05-11 02:33:29 +0800

    brainstorming on portable abstraction for types

--- src/typed/class.clj
+++ src/typed/class.clj
@@ -4,195 +4,113 @@
                          IMapEntry AMapEntry MapEntry ILookup Associative IPersistentMap
                          IDeref IMeta IObj IRef IReference AReference ARef Atom Ref ISeq
                          IPersistentSet Delay Agent IBlockingDeref IEditableCollection
-                         IHashEq PersistentHashMap PersistentTreeSet PersistentHashSet))
-  (:require [typed.core :refer [annotate-class]]))
+                         IHashEq PersistentHashMap PersistentTreeSet PersistentHashSet Ratio
+                         IFn Fn)))
+
+;markers with no equivalents
+(def-type ivar)
+
+;numbers
+(def-type-alias integer (U Integer LongDouble clojure.lang.BigInt BigInteger Short Byte))
+(def-type-alias ratio Ratio)
+(def-type-alias decimal BigDecimal)
+(def-type-alias rational (U integer ratio decimal))
+(def-type-alias float (U Double Float))
+(def-type-alias number Number)
+
+;collection interfaces
+(def-type-alias sorted Sorted)
+(def-type-alias sequential Sequential)
+(def-type-alias reversible Reversible)
+(def-type-alias counted Counted)
+(def-type-alias hash IHashEq)
+(def-type-alias ilookup ILookup)
+(def-type-alias named Named)
+(def-type-alias ivector IPersistentVector)
+(def-type-alias imap IPersistentMap)
+(def-type-alias iset IPersistentSet)
+(def-type-alias iseq ISeq)
+(def-type-alias iseqable Seqable)
+(def-type-alias icoll IPersistentCollection)
+(def-type-alias ilist IPersistentList)
+(def-type-alias iset IPersistentSet)
+(def-type-alias ifn IFn)
+(def-type-alias fn Fn)
+
+;single types
+(def-type-alias char Character)
+
+;collection types
+(def-type-alias string String)
+
+(def-type-alias nonseqable-seqable
+                (U Iterable
+                   ;array
+                   CharSequence
+                   java.util.Map))
+
+(def-type-alias (seqable a)
+                (I iseqable
+                   nonseqable-seqable))
+
+(def-type-alias (seq a)
+                (I iseq
+                   (seqable a)
+                   (coll a)
+                   sequential
+                   hash))
+
+(def-type-alias (map a b)
+                (I imap
+                   (seqable (map-entry a b))
+                   (coll (map-entry a b))
+                   (All [x]
+                     (Fun [Any -> (U nil b)]
+                          [Any x -> (U x b)]))
+                   hash))
+
+(def-type-alias (vector a)
+                (I ivector
+                   (seqable a)
+                   [integer -> a]
+                   (coll a)
+                   (associative integer a)))
+
+(def-type-alias (coll a)
+                (I icoll
+                   (seqable a)))
 
 
-;interfaces, like protocols here. Mix and match, but none imply others.
+(def-type-alias (list a)
+                (I ilist
+                   (seqable a)
+                   (seq a)
+                   (coll a)
+                   (stack a)))
 
-(parameterise-interface Seqable [a])
+(def-type-alias (set a)
+                (I iset
+                   (seqable a)
+                   counted))
 
-(parameterise-interface IPersistentCollection [a])
+(def-type-alias (stack a)
+                (I istack
+                   (seqable a)
+                   sequential))
 
-(parameterise-interface ISeq [a])
+(def-type-alias symbol
+                (I isymbol
+                   named
+                   (All [k v o]
+                     (Fun [(map k v) -> (U nil v)]
+                          [(map k v) o -> (U o v)]))
+                   meta))
 
-(parameterise-interface IPersistentStack [a])
+(def-type-alias keyword
+                (I ikeyword
+                   named
+                   (Fun [(map k v) -> (U nil v)]
+                        [(map k v) o -> (U o v)])))
 
-(parameterise-interface IPersistentSet [a])
-
-(parameterise-interface IPersistentList [a])
-
-(parameterise-interface IPersistentVector [a])
-
-(parameterise-interface IMapEntry [a b])
-
-(parameterise-interface ILookup [a b])
-
-(parameterise-interface Associative [a b])
-
-(parameterise-interface IPersistentMap [a b])
-
-(parameterise-interface IDeref [a])
-
-(parameterise-interface IBlockingDeref [a])
-
-(parameterise-interface IMeta [(a <! (U nil (IPersistentMap Any Any)))])
-
-;abstract classes, more like type aliases here
-
-(parameterise-abstract-class APersistentMap [a b]
-  (I Iterable 
-     (IPersistentCollection (MapEntryT a b))
-     IHashEq 
-     [Any -> (U nil b)] 
-     (Associative a b)
-     MapEquivalence 
-     java.util.concurrent.Callable 
-     (clojure.lang.IPersistentMap a b)
-     (ILookup a b)
-     java.io.Serializable
-     java.lang.Runnable 
-     java.util.Map 
-     Counted 
-     (Seqable (MapEntryT a b))
-     java.lang.Object))
-
-(parameterise-abstract-class APersistentVector [a]
-  (I Reversible 
-     Indexed 
-     Iterable 
-     (IPersistentCollection a) 
-     IHashEq 
-     [Long -> a] 
-     (Associative Long a) 
-     Comparable 
-     RandomAccess 
-     java.util.concurrent.Callable 
-     (IPersistentVector a) 
-     java.util.List 
-     java.util.Collection 
-     (ILookup Long a) 
-     Serializable 
-     Runnable 
-     Counted 
-     (Seqable a)
-     Object 
-     (IPersistentStack a)
-     Sequential 
-     IEditableCollection))
-
-(parameterise-abstract-class AMapEntry [a b]
-  (I Reversible 
-     Indexed 
-     Iterable 
-     (IPersistentCollection (U a b))
-     IHashEq 
-     [Long -> (U a b)]
-     (Associative Long (U a b))
-     java.lang.Comparable 
-     java.util.RandomAccess 
-     java.util.concurrent.Callable 
-     (IPersistentVector (U a b))
-     java.util.List 
-     java.util.Collection 
-     (ILookup Long (U a b))
-     java.io.Serializable 
-     java.util.Map$Entry 
-     (IMapEntry a b) 
-     java.lang.Runnable 
-     Counted 
-     (Seqable (U a b)) 
-     Object 
-     (IPersistentStack (U a b))
-     Sequential))
-
-(parameterise-abstract-class APersistentSet [a]
-  (I (IPersistentSet a)
-     [Any -> (U nil a)]
-     Iterable 
-     (IPersistentCollection a) 
-     IHashEq 
-     java.util.Set 
-     java.util.concurrent.Callable 
-     java.util.Collection 
-     java.io.Serializable 
-     java.lang.Runnable 
-     Counted 
-     (Seqable a) 
-     java.lang.Object))
-
-(parameterise-abstract-class Obj [a]
-  (I IObj
-     (IMeta a)
-     java.io.Serializable))
-
-(parameterise-abstract-class ASeq [a]
-  (I java.lang.Iterable 
-     (IPersistentCollection a) 
-     IHashEq 
-     java.util.List 
-     java.util.Collection 
-     java.io.Serializable 
-     (ISeq a)
-     (Seqable a)
-     java.lang.Object 
-     Sequential 
-     (Obj (APersistentMap Any Any))))
-
-;concrete classes, again more like type aliases. Abstract classes useful for abstraction
-
-(parameterise-class PersistentVector [a]
-  (I IObj
-     (IMeta (Map Any Any))
-     (APersistentVector a)))
-
-(parameterise-class PersistentHashMap [a b]
-  (I (Map a b)
-     IObj
-     (IMeta (Map Any Any))
-     IEditableCollection))
-
-(parameterise-class PersistentTreeMap [a b]
-  (I (Map a b) 
-     IObj
-     (IMeta (Map Any Any))
-     Reversible 
-     Sorted))
-
-(parameterise-class PersistentHashSet [a]
-  (I IEditableCollection
-     (IMeta (Map Any Any))
-     IObj))
-
-(parameterise-class PersistentTreeSet [a]
-  (I Reversible
-     Sorted
-     (IMeta (Map Any Any))
-     IObj))
-
-(parameterise-class MapEntry [a b]
-  (MapEntryT a b))
-
-(parameterise-class Keyword []
-  (I (All [k v o] 
-       (Fun [(Map k v) -> (U nil v)]
-            [(Map k v) o -> (U o v)]))
-     java.lang.Comparable 
-     java.util.concurrent.Callable 
-     java.io.Serializable 
-     java.lang.Runnable 
-     Named 
-     java.lang.Object))
-
-(parameterise-class Symbol []
-  (I (All [k v o] 
-       (Fun [(Map k v) -> (U nil v)]
-            [(Map k v) o -> (U o v)]))
-     (IMeta (Map Any Any))
-     IObj
-     java.lang.Comparable 
-     java.util.concurrent.Callable 
-     java.io.Serializable 
-     java.lang.Runnable 
-     Named 
-     java.lang.Object)
+fn
+future
