126be417e76ce390fff1e18853fba45db31fe652
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2012-05-19 01:10:22 +0800

    constraint generation

--- src/typed/core.clj
+++ src/typed/core.clj
@@ -12,42 +12,19 @@
         rntime-class (:class opts)]
     `(def ~nme 
        (let [tc-name# (qualify-symbol '~nme)]
-         (reify
-           ITCType
-           ITypeName
-           (~'-type-name [this#]
-             tc-name#)
-           ISuperTypes
-           (~'-supertype-of [this#]
-             ~type)
-           ~@(when rntime-class
-               (list 'IRuntimeClass
-                     `(~'-runtime-class [this#]
-                          ~rntime-class))))))))
+         (->NewType '~nme ~type ~rntime-class)))))
 
-(declare parse-tvars tv-from-syntax)
+(declare tv-from-syntax)
 
 (defmacro All [tvars type]
   (let [tvar-bndings (map first tvars)
         tvar-objs (mapv tv-from-syntax tvars)]
-    `(reify
-       IPolyConstructor
-       (~'-tvars [this#]
-         ~tvar-objs)
-
-       (~'-poly-type [this#]
-         (let [~@(mapcat #(vector %1 %2) tvar-bndings tvar-objs)]
-           ~type))
-
-       (~'-inst [const# types#]
-         (reify 
-           ITCType
-           IPolyInst
-           (~'-constructed-from [this#]
-             const#)
-           ISuperTypes
-           (~'-supertype-of [this#]
-             (throw (Exception. ""))))))))
+    `(->PolyConstructor
+       nil
+       ~tvar-objs
+       (let [~@(mapcat #(vector %1 %2) tvar-bndings tvar-objs)]
+         ~type)
+       nil)))
 
 (defmacro def-poly-type [nme tvars type & opts]
   (let [tvar-bndings (map first tvars)
@@ -56,33 +33,13 @@
         rntime-class (:class opts)]
     `(def ~nme 
        (let [type-nme# (qualify-symbol '~nme)]
-         (reify
-           ITypeName
-           (~'-type-name [this#]
-             type-nme#)
-           IPolyConstructor
-           (~'-tvars [this#]
-             ~tvar-objs)
+         (->PolyConstructor
+           '~nme
+           ~tvar-objs
+           (let [~@(mapcat #(vector %1 %2) tvar-bndings tvar-objs)]
+             ~type)
+           ~rntime-class)))))
 
-           (~'-poly-type [this#]
-             (let [~@(mapcat #(vector %1 %2) tvar-bndings tvar-objs)]
-               ~type))
-
-           (~'-inst [const# types#]
-             (reify 
-               ITCType
-               ITypeName
-               (~'-type-name [this#]
-                 type-nme#)
-               IPolyInst
-               (~'-constructed-from [this#]
-                 const#)
-               ISuperTypes
-               (~'-supertype-of [this#]
-                 (throw (Exception. "")))
-               IRuntimeClass
-               (~'-runtime-class [this#]
-                 ~rntime-class))))))))
 
 (defn qualify-symbol [sym]
   (if (namespace sym)
@@ -133,6 +90,48 @@
 
 ;; Type Rep
 
+(deftype PolyInstance [nme constructor type rntime-class]
+  ITCType
+  ITypeName
+  (-type-name [this]
+    nme)
+  IPolyInst
+  (-constructed-from [this]
+    constructor)
+  ISuperTypes
+  (-supertype-of [this]
+    (throw (Exception. "")))
+  IRuntimeClass
+  (-runtime-class [this]
+    rntime-class))
+
+(deftype PolyConstructor [nme tvars type rntime-class]
+  ITypeName
+  (-type-name [this]
+    nme)
+
+  IPolyConstructor
+  (-tvars [this]
+    tvars)
+  (-poly-type [this]
+    type)
+
+  (-inst [this types]
+    (throw (Exception.))
+    (->PolyInstance nme this 'FIXME rntime-class))) ;TODO substitution
+
+(deftype NewType [nme type rntime-class]
+  ITCType
+  ITypeName
+  (-type-name [this]
+    nme)
+  ISuperTypes
+  (-supertype-of [this]
+    type)
+  IRuntimeClass
+  (-runtime-class [this#]
+    rntime-class))
+
 (deftype FunctionType [dom rng has-uniform-rest uniform-rest]
   Object
   (toString [this]
@@ -153,9 +152,16 @@
   (toString [this]
     (str (apply list 'I types))))
 
+(defn poly? [a]
+  (instance? PolyConstructor a))
+(defn function? [a]
+  (instance? FunctionType a))
+(defn tv? [a]
+  (instance? tv? a))
+
 ;; Type Constructors
 
-(declare massage-function-syntax normalise-tv-syntax tv-from-syntax)
+(declare massage-function-syntax)
 
 (defmacro Fn [& arities]
   `(apply I (map function-from-syntax ~(mapv massage-function-syntax arities))))
@@ -196,6 +202,326 @@
                 (bound-syn opts)
                 `Any)]
     `(->TV '~(gensym nme) ~bound ~variance)))
+
+;; Visit tvar
+
+(defprotocol IVarVisit
+  (-visit-tvar [this f]))
+
+(extend-protocol IVarElim
+  NewType
+  (-visit-tvar [this f]
+    (->NewType 
+      (.nme this)
+      (-visit-tvar (.type this) f)
+      (.rntime-class this)))
+
+  PolyInstance
+  (-visit-tvar [this f]
+    (->PolyInstance
+      (.nme this)
+      (-visit-tvar (.constructor this) f)
+      (-visit-tvar (.type this) f)
+      (.rntime-class this)))
+
+  PolyConstructor
+  (-visit-tvar [this f]
+    (->PolyConstructor
+      (.nme this)
+      (map -visit-tvar (.tvars this) (repeat f))
+      (-visit-tvar (.type this) f)
+      (.rntime-class this)))
+
+  TV
+  (-visit-tvar [this f]
+    (f this))
+
+  UnionType
+  (-visit-tvar [this f]
+    (->UnionType
+      (map -visit-tvar (.types this) (repeat f))))
+
+  FunctionType
+  (-visit-tvar [this f]
+    (->FunctionType
+      (map -demote (.dom this) (repeat f))
+      (-visit-tvar (.rng this) f)
+      (.has-uniform-rest this)
+      (if (.has-uniform-rest this)
+        (-demote (.uniform-rest this) f)
+        (.uniform-rest this)))))
+
+;; Variable Elimination
+
+(defprotocol IVarElim
+  (-promote [this vs])
+  (-demote [this vs]))
+
+(extend-protocol IVarElim
+  NewType
+  (-promote [this vs]
+    (->NewType 
+      (.nme this)
+      (-promote (.type this) vs)
+      (.rntime-class this)))
+  (-demote [this vs]
+    (->NewType 
+      (.nme this)
+      (-demote (.type this) vs)
+      (.rntime-class this)))
+
+  PolyInstance
+  (-promote [this vs]
+    (->PolyInstance
+      (.nme this)
+      (-promote (.constructor this) vs)
+      (-promote (.type this) vs)
+      (.rntime-class this)))
+  (-demote [this vs]
+    (->PolyInstance
+      (.nme this)
+      (-demote (.constructor this) vs)
+      (-demote (.type this) vs)
+      (.rntime-class this)))
+
+  PolyConstructor
+  (-promote [this vs]
+    (->PolyConstructor
+      (.nme this)
+      (.tvars this) ;type variables are uniquely named, guaranteed not in vs
+      (-promote (.type this) vs)
+      (.rntime-class this)))
+  (-demote [this vs]
+    (->PolyConstructor
+      (.nme this)
+      (.tvars this) ;type variables are uniquely named, guaranteed not in vs
+      (-demote (.type this) vs)
+      (.rntime-class this)))
+
+  TV
+  (-promote [this vs]
+    (if (contains? vs this)
+      (.bound this)
+      this))
+  (-demote [this vs]
+    (if (contains? vs this)
+      Nothing
+      this))
+
+  UnionType
+  (-promote [this vs]
+    (->UnionType
+      (map -promote (.types this) (repeat vs))))
+  (-demote [this vs]
+    (->UnionType
+      (map -demote (.types this) (repeat vs))))
+
+  FunctionType
+  (-promote [this vs]
+    (->FunctionType
+      (map -demote (.dom this) (repeat vs))
+      (-promote (.rng this) vs)
+      (.has-uniform-rest this)
+      (if (.has-uniform-rest this)
+        (-demote (.uniform-rest this) vs)
+        (.uniform-rest this))))
+  (-demote [this vs]
+    (->FunctionType
+      (map -promote (.dom this) (repeat vs))
+      (-demote (.rng this) vs)
+      (.has-uniform-rest this)
+      (if (.has-uniform-rest this)
+        (-promote (.uniform-rest this) vs)
+        (.uniform-rest this)))))
+
+;; Constraint Generation
+
+(deftype EqConstraint [type])
+(deftype SubConstraint [lower upper])
+
+(defmulti intersect-cs-fn (fn [a b] [(class a) (class b)]))
+
+(defmethod intersect-cs-fn [EqConstraint EqConstraint]
+  [a b]
+  (assert (= a b))
+  a)
+
+(defmethod intersect-cs-fn [EqConstraint SubConstraint]
+  [a b]
+  (assert (and (subtype? (.lower b) (.type a))
+               (subtype? (.type a) (.upper b))))
+  a)
+
+(defmethod intersect-cs-fn [SubConstraint EqConstraint]
+  [b a]
+  (assert (and (subtype? (.lower b) (.type a))
+               (subtype? (.type a) (.upper b))))
+  a)
+
+(defmethod intersect-cs-fn [SubConstraint SubConstraint]
+  [b a]
+  (let [j (U (.lower a) (.lower b))
+        m (I (.upper a) (.upper b))]
+    (->SubConstraint j m)))
+
+(defn intersect-cs [& cs]
+  (merge-with intersect-cs-fn cs))
+
+(defn empty-cs [xs]
+  (zipmap xs (repeat (->SubConstraint Nothing Any))))
+(defn singleton-cs [xs x c]
+  (intersect-cs (empty-cs xs) {x c}))
+
+(defn matching-rel 
+  "Returns [r cs] where r is a type such that sub == type,
+  and cs are the constraints"
+  [sub type xs]
+  (cond
+    (and (= Any sub)
+         (= Any type))
+    [Any (empty-cs xs)]
+
+    (and (= Nothing sub)
+         (= Nothing type))
+    [Nothing (empty-cs xs)]
+
+    (and (tv? sub)
+         (contains? xs sub))
+    [type (singleton-cs xs sub (->EqConstraint type))]
+
+    (and (tv? type)
+         (contains? xs type))
+    [sub (singleton-cs xs type (->EqConstraint sub))]
+
+    (and (tv? sub)
+         (tv? type)
+         (= sub type))
+    [sub (empty-cs xs)]
+
+    (and (poly? sub)
+         (poly? type)
+         (= (map #(.nme %) (-tvars sub))
+            (map #(.nme %) (-tvars type))))
+    (let [sub-tvars (-tvars sub)
+          type-tvars (-tvars type)
+          _ (assert (= sub-tvars type-tvars))
+          [new-tvars ds] (let [res (map #(matching-rel (.bound %1) (.bound %2) xs)
+                                        sub-tvars
+                                        type-tvars)
+                               ks-bnds (map first res)
+                               new-tvars (map #(->TV
+                                                 (.nme %1)
+                                                 %2
+                                                 (.variance %2))
+                                              sub-tvars
+                                              ks-bnds)
+                               ds (intersect-cs (map second res))]
+                           [new-tvars ds])
+          rplc-fn (fn [old]
+                    (if-let [ntvar (some #(.nme new-tvars) old)]
+                      (->TV
+                        (.nme old)
+                        (.bound ntvar)
+                        (.variance old))
+                      old))
+          sub-rplced (-visit-tvar (-poly-type sub) rplc-fn)
+          type-rplced (-visit-tvar (-poly-type type) rplc-fn)
+          [mtch-type cs] (matching-rel sub-rplced type-rplced xs)
+          all-cs (intersect-cs cs ds)]
+      [(->PolyConstructor
+         (.nme sub)
+         new-tvars
+         mtch-type
+         (.rntime-class sub))
+       all-cs])
+
+    (and (function? sub)
+         (function? type))
+    (let [rs (.dom sub)
+          s (.rng sub)
+          ts (.dom type)
+          u (.rng type)
+          rest-sub (.uniform-rest sub)
+          rest-type (.uniform-rest type)
+          [ls cs] (let [res (map #(matching-rel %1 %2 xs)
+                                 ts
+                                 rs)
+                        ls (map first res)
+                        cs (apply intersect-cs (map second res))]
+                    [ls cs])
+          [m d] (matching-rel s u xs)
+          [rest e] (when (.has-uniform-rest sub)
+                     (matching-rel rest-sub rest-type xs))]
+      [(->FunctionType
+         ls
+         m
+         (.has-uniform-rest sub)
+         rest)
+       (apply intersect-cs (concat [d] (when e [e]) cs))])))
+
+(defn gen-constraint [sub type xs]
+  (cond
+    (or (= Any type)
+        (= Nothing sub))
+    (empty-cs xs)
+
+    (and (tv? sub)
+         (tv? type)
+         (= sub type))
+    (empty-cs xs)
+
+    (and (tv? sub)
+         (contains? xs sub))
+    (let [r (-demote type xs)]
+      (singleton-cs xs sub (->SubConstraint Nothing r)))
+
+    (and (tv? type)
+         (contains? xs type))
+    (let [r (-promote sub xs)]
+      (singleton-cs xs type (->SubConstraint r Any)))
+
+    (tv? sub)
+    (gen-constraint (.bound sub) type xs)
+
+    (and (poly? sub)
+         (poly? type)
+         (= (map #(.nme %) (-tvars sub))
+            (map #(.nme %) (-tvars type))))
+    (let [res (map #(matching-rel (.bound %1) (.bound %2) xs)
+                   (-tvars sub)
+                   (-tvars type))
+          ks (map first res)
+          ds (map second res)
+
+          rplc-fn (fn [old]
+                    (if-let [t (some #(.nme old) (-tvars sub))]
+                      (->TV
+                        (.nme old)
+                        (.bound t)
+                        (.variance old))
+                      old))
+          cs (gen-constraint (-visit-tvar (.type sub) rplc-fn)
+                             (-visit-tvar (.type type) rplc-fn))]
+      (apply intersect-cs cs ds))
+
+    (and (function? sub)
+         (function? type))
+    (let [_ (assert (= (.has-uniform-rest sub) (.has-uniform-rest type))) ;assume both have rest or both don't
+          rs (.dom sub)
+          s (.rng sub)
+          ts (.dom type)
+          u (.rng type)
+          rest-sub (.uniform-rest sub)
+          rest-type (.uniform-rest type)
+          _ (assert (= (count ts) (count rs))) ;assume same length args always
+          cs (apply intersect-cs 
+                    (map #(gen-constraint %1 %2 xs)
+                         ts
+                         rs))
+          d (gen-constraint s u xs)
+          e (when (.has-uniform-rest sub)
+              (gen-constraint rest-type rest-sub xs))]
+      (apply intersect-cs (concat d (when e [e]) cs)))))
 
 ;; Subtyping
 
@@ -255,68 +581,68 @@
 (def-poly-type NonEmptySeqable [[a :variance :covariant]]
                Nothing)
 
-(def-poly-type Seqable [[a :variance :covariant]]
-               (U (Inst NonEmptySeqable a)
-                  EmptySeqable))
-
-(def-poly-type Option [[a :variance :covariant]]
-               (U a nil))
-
-(def-type EmptyColl
-          Nothing
-          :class clojure.lang.IPersistentCollection)
-
-(def-poly-type NonEmptyColl [[a :variance :covariant]]
-               Nothing
-               :class clojure.lang.IPersistentCollection)
-
-(def-poly-type Coll [[a :variance :covariant]]
-               (I (U EmptyColl
-                     (Inst NonEmptyColl a))
-                  (Inst Seqable a))
-               :class clojure.lang.IPersistentCollection)
-
-(def-type EmptySeq 
-          EmptySeqable
-          :class clojure.lang.ISeq)
-
-(def-poly-type NonEmptySeq [[a :variance :covariant]]
-               (Inst NonEmptySeqable a)
-               :class clojure.lang.ASeq)
-
-(def-poly-type Seq [[a :variance :covariant]]
-               (I (U EmptySeq
-                     (Inst NonEmptySeq a))
-                  (Inst Seqable a)
-                  (Inst Coll a))
-               :class clojure.lang.ISeq)
-
-(def-type EmptyVector
-          EmptySeqable
-          :class clojure.lang.IPersistentVector)
-
-(def-poly-type NonEmptyVector [[a :variance :covariant]]
-               (Inst NonEmptySeqable a)
-               :class clojure.lang.IPersistentVector)
-
-(def-poly-type Vector [[a :variance :covariant]]
-               (I (U EmptyVector
-                     (Inst NonEmptyVector a))
-                  (Inst Seqable a)
-                  (Inst Coll a))
-               :class clojure.lang.IPersistentVector)
-
-(+T clojure.core/seq
-    (All [[x :variance :covariant]]
-         (Fn [(EmptySeqable x) -> nil]
-             [(NonEmptySeqable x) -> (Seq x)]
-             [(Seqable x) -> (U (Seq x) nil)])))
-
-(+T clojure.core/first
-    (All [[x :variance :covariant]]
-         (Fn [EmptySeqable -> nil]
-             [(NonEmptySeqable x) -> x]
-             [(Seqable x) -> (U x nil)])))
+;(def-poly-type Seqable [[a :variance :covariant]]
+;               (U (Inst NonEmptySeqable a)
+;                  EmptySeqable))
+;
+;(def-poly-type Option [[a :variance :covariant]]
+;               (U a nil))
+;
+;(def-type EmptyColl
+;          Nothing
+;          :class clojure.lang.IPersistentCollection)
+;
+;(def-poly-type NonEmptyColl [[a :variance :covariant]]
+;               Nothing
+;               :class clojure.lang.IPersistentCollection)
+;
+;(def-poly-type Coll [[a :variance :covariant]]
+;               (I (U EmptyColl
+;                     (Inst NonEmptyColl a))
+;                  (Inst Seqable a))
+;               :class clojure.lang.IPersistentCollection)
+;
+;(def-type EmptySeq 
+;          EmptySeqable
+;          :class clojure.lang.ISeq)
+;
+;(def-poly-type NonEmptySeq [[a :variance :covariant]]
+;               (Inst NonEmptySeqable a)
+;               :class clojure.lang.ASeq)
+;
+;(def-poly-type Seq [[a :variance :covariant]]
+;               (I (U EmptySeq
+;                     (Inst NonEmptySeq a))
+;                  (Inst Seqable a)
+;                  (Inst Coll a))
+;               :class clojure.lang.ISeq)
+;
+;(def-type EmptyVector
+;          EmptySeqable
+;          :class clojure.lang.IPersistentVector)
+;
+;(def-poly-type NonEmptyVector [[a :variance :covariant]]
+;               (Inst NonEmptySeqable a)
+;               :class clojure.lang.IPersistentVector)
+;
+;(def-poly-type Vector [[a :variance :covariant]]
+;               (I (U EmptyVector
+;                     (Inst NonEmptyVector a))
+;                  (Inst Seqable a)
+;                  (Inst Coll a))
+;               :class clojure.lang.IPersistentVector)
+;
+;(+T clojure.core/seq
+;    (All [[x :variance :covariant]]
+;         (Fn [(EmptySeqable x) -> nil]
+;             [(NonEmptySeqable x) -> (Seq x)]
+;             [(Seqable x) -> (U (Seq x) nil)])))
+;
+;(+T clojure.core/first
+;    (All [[x :variance :covariant]]
+;         (Fn [EmptySeqable -> nil]
+;             [(NonEmptySeqable x) -> x]
+;             [(Seqable x) -> (U x nil)])))
 
 ;; Checker
 
