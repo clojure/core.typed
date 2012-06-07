1a9167efdc6bd08af4d9647234110ae54a642522
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2012-06-08 03:39:25 +0800

    RClass, RInstance

--- src/typed/new.clj
+++ src/typed/new.clj
@@ -1,5 +1,7 @@
 (ns typed.new
-  (:refer-clojure :exclude [defrecord])
+  (:refer-clojure :exclude [defrecord type])
+  (:import (clojure.lang IPersistentList IPersistentVector Symbol Cons Seqable IPersistentCollection
+                         ISeq ASeq ILookup))
   (:require [analyze.core :as analyze]
             [clojure.set :as set]
             [trammel.core :as contracts]
@@ -22,13 +24,10 @@
 
 (def nat? (every-pred (complement neg?) integer?))
 
-(def kinds #{:B :F :RClass :Poly :Mu :Union :intersection
-             :Top})
-
 (def Type ::Type)
 
 (defn Type? [a]
-  (isa? a Type))
+  (isa? (class a) Type))
 
 (defn declare-type [a]
   (derive a Type))
@@ -39,6 +38,7 @@
 
 (declare-type Top)
 
+;TODO flatten unions
 (defrecord Union [types]
   "An unordered union of types"
   [(every? Type? types)])
@@ -55,34 +55,94 @@
 
 (declare-type Intersection)
 
-(defrecord B [idx]
+(def variances #{:constant :covariant :contravariant :invariant})
+
+(defn variance? [v]
+  (contains? variances v))
+
+(defrecord B [idx upper-bound lower-bound variance]
   "A bound variable. Should not appear outside this file"
-  [(nat? idx)])
+  [(nat? idx)
+   (or (nil? upper-bound)
+       (Type? upper-bound))
+   (or (nil? lower-bound)
+       (Type? lower-bound))
+   (or (nil? variance)
+       (variance? variance))])
 
 (declare-type B)
 
-(defrecord F [name]
+(defrecord F [name upper-bound lower-bound variance]
   "A named free variable"
-  [(symbol? name)])
+  [(symbol? name)
+   (or (nil? upper-bound)
+       (Type? upper-bound))
+   (or (nil? lower-bound)
+       (Type? lower-bound))
+   (or (nil? variance)
+       (variance? variance))])
 
 (declare-type F)
 
-(defrecord RClass [the-class replacements]
+(defrecord RClass [variances the-class replacements]
   "A restricted class, where ancestors are
   (replace replacements (ancestors the-class))"
-  [(class? the-class)
+  [(or (nil? variances)
+       (and (sequential? variances)
+            (every? #(or (nil? %)
+                         (variance? %))
+                    variances)))
+   (class? the-class)
+   (map? replacements)
    (every? class? (keys replacements))
-   (every? Type? (vals replacements))])
+   (every? (some-fn Type? Scope?) (vals replacements))])
+
+;smart constructor
+(defn RClass* [tparams the-class replacements]
+  (if (seq tparams)
+    (->RClass (doall (map :variance tparams))
+              the-class
+              (into {} (for [[k v] replacements]
+                         [k (abstract-many (map :name tparams) v)])))
+    (->RClass nil the-class replacements)))
 
 (declare-type RClass)
+
+(defrecord RInstance [poly? constructor]
+  "An instance of a class"
+  [(or (nil? poly?)
+       (and (sequential? poly?)
+            (every? Type? poly?)))
+   (RClass? constructor)])
+
+(declare-type RInstance)
+
+(defrecord Field [name type]
+  "A datatype/record field"
+  [(symbol? name)
+   (Type? type)])
+
+(defrecord Record [the-class fields]
+  "A record"
+  [(class? the-class)
+   (every? Field? fields)])
+
+(declare-type Record)
+
+(defrecord DataType [the-class fields]
+  "A Clojure datatype"
+  [(class? the-class)
+   (every? Field? fields)])
+
+(declare-type DataType)
 
 (defrecord Scope [body]
   "A scope that contains bound variables. Not used directly"
   [(Type? body)])
 
-(defrecord Poly [n scope]
+(defrecord Poly [nbound scope]
   "A polymorphic type containing n bound variables"
-  [(nat? n)
+  [(nat? nbound)
    (Scope? scope)])
 
 (declare-type Poly)
@@ -95,17 +155,211 @@
     body
     (->Poly (count names) (abstract-many names body))))
 
+(defrecord PolyDots [n scope]
+  "A polymorphic type containing n-1 bound variables and 1 ... variable"
+  [(nat? n)
+   (Scope? scope)])
+
+(declare-type PolyDots)
+
+;smart constructor
+(defn PolyDots* [names body]
+  {:pre [(every? symbol names)
+         (Type? body)]}
+  (if (empty? names)
+    body
+    (->PolyDots (count names) (abstract-many names body))))
+
 (defrecord Mu [scope]
   "A recursive type containing one bound variable, itself"
   [(Scope? scope)])
 
 (declare-type Mu)
 
+(defrecord Function [dom rng rest drest]
+  "A function arity"
+  [(sequential? dom)
+   (every? Type? dom)
+   (or (nil? rest)
+       (Type? rest))
+   (or (nil? drest)
+       (Type? drest))])
+
+(declare-type Function)
+
 (declare abstract)
 
 ;smart constructor
 (defn Mu* [name body]
   (->Mu (abstract name body)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Restricted Class
+
+;Class -> RClass
+(def RESTRICTED-CLASS (atom {}))
+
+(declare parse-binder with-frees)
+
+(defn- build-replacement-syntax [m]
+  (into {} (for [[k v] m]
+             [k `(parse-type '~v)])))
+
+(defmacro alter-class [the-class frees-syn & opts]
+  (let [{replacements-syn :replace} (apply hash-map opts)
+        replacements (build-replacement-syntax replacements-syn)]
+     `(let [frees# (when-let [fs# (seq '~frees-syn)]
+                     (parse-binder fs#))]
+        (swap! RESTRICTED-CLASS 
+               #(assoc % ~the-class (RClass* frees# ~the-class (with-frees frees#
+                                                                   ~replacements))))
+        ~the-class)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Type syntax
+
+;(Map Symbol F)
+(def ^:dynamic *free-scope* {})
+
+(defmacro with-frees [frees & body]
+  `(let [m# (zipmap (map :name ~frees) ~frees)]
+     (binding [*free-scope* (merge *free-scope* m#)]
+       ~@body)))
+
+(defmulti parse-type class)
+(defmulti parse-type-list first)
+
+(defn parse-free [f]
+  (if (symbol? f)
+    (->F f nil nil nil)
+    (let [[n & opts] f
+          {upp :<
+           low :>
+           variance :variance} (apply hash-map opts)]
+      (->F n 
+           (when upp 
+             (parse-type upp)) 
+           (when low
+             (parse-type low))
+           variance))))
+
+(defn check-forbidden-rec [rec tbody]
+  (when (or (= rec tbody) 
+            (and (Intersection? tbody)
+                 (contains? (set (:types tbody)) rec))
+            (and (Union? tbody)
+                 (contains? (set (:types tbody)) rec)))
+    (throw (Exception. "Recursive type not allowed here"))))
+
+(defn parse-rec-type [[rec [free-symbol :as bnder] type]]
+  (let [_ (assert (= 1 (count bnder)) "Only one variable in allowed: Rec")
+        f (->F free-symbol nil nil nil)
+        body (with-frees [f]
+               (parse-type type))
+        
+        _ (check-forbidden-rec f body)]
+    (Mu* (:name f) body)))
+
+(defmethod parse-type-list 'Rec
+  [syn]
+  (parse-rec-type syn))
+
+(defn parse-binder [bnds]
+  (let [scope (atom [])]
+    (doall
+      (for [bnd bnds]
+        (let [f (with-frees @scope
+                  (parse-free bnd))
+              _ (swap! scope #(conj % f))]
+          f)))))
+
+(defn parse-all-type [[all bnds type]]
+  (let [frees (parse-binder bnds)]
+    (Poly* (map :name frees)
+           (with-frees frees
+             (parse-type type)))))
+
+(defmethod parse-type-list 'All
+  [syn]
+  (parse-all-type syn))
+
+(defn parse-union-type [[u & types]]
+  (->Union (doall (map parse-type types))))
+
+(defmethod parse-type-list 'U
+  [syn]
+  (parse-union-type syn))
+
+(defn parse-intersection-type [[i & types]]
+  (->Intersection (doall (map parse-type types))))
+
+(defmethod parse-type-list 'I
+  [syn]
+  (parse-intersection-type syn))
+
+(defmethod parse-type-list :default
+  [[cls-sym & params-syn]]
+  (let [cls (resolve cls-sym)
+        rclass (@RESTRICTED-CLASS cls)
+        _ (assert rclass (str cls " not declared as polymorphic"))
+        tparams (doall (map parse-type params-syn))]
+    (->RInstance tparams rclass)))
+
+(defmethod parse-type Cons
+  [l]
+  (parse-type-list l))
+
+(defmethod parse-type IPersistentList
+  [l]
+  (parse-type-list l))
+
+(defmulti parse-type-symbol identity)
+(defmethod parse-type-symbol 'Any [_] (->Top))
+(defmethod parse-type-symbol 'Nothing [_] (Bottom))
+
+(defmethod parse-type-symbol :default
+  [sym]
+  (cond
+    (sym *free-scope*) (sym *free-scope*)
+
+    :else
+    (let [res (resolve sym)]
+      (cond 
+        ;make instance, must provide type parameters if any
+        (class? res) (let [rclass (@RESTRICTED-CLASS res)
+                           _ (assert (not (:variances rclass)) (str "RClass " res " must be instantiated"))]
+                       (->RInstance nil (or rclass (->RClass nil res {}))))
+
+        :else (throw (Exception. (str "Cannot resolve type: " res)))))))
+
+(defmethod parse-type Symbol
+  [l]
+  (parse-type-symbol l))
+
+(defn parse-function [f]
+  (let [[all-dom _ [rng]] (partition-by #(= '-> %) f)
+
+        _ (assert (not (and (some #(= '& %) all-dom)
+                            (some #(= '... %) all-dom)))
+                  "Cannot provide both rest type and dotted rest type")
+
+        fixed-dom (take-while #(not (or (= '& %)
+                                        (= '... %)))
+                              all-dom)
+
+        [_ rest-type] (drop-while #(not= '& %) all-dom)
+        [_ drest-type] (drop-while #(not= '... %) all-dom)]
+    (->Function (doall (map parse-type fixed-dom))
+                (parse-type rng) 
+                (when rest-type
+                  (parse-type rest-type))
+                (when drest-type
+                  (parse-type drest-type)))))
+
+(defmethod parse-type IPersistentVector
+  [f]
+  (parse-function f))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Variable rep
@@ -114,18 +368,20 @@
   "Wrap type in n Scopes"
   {:pre [(nat? n)
          (Type? t)]}
-  (last 
-    (take (inc n) (iterate Scope t))))
+  (doall
+    (last 
+      (take (inc n) (iterate ->Scope t)))))
 
 (defn remove-scopes [n sc]
   "Unwrap n Scopes"
   {:pre [(nat? n)
          (Type? sc)]}
-  (last
-    (take (inc n) (iterate (fn [{body :body :as t}]
-                             (assert (Scope? t) "Tried to remove too many Scopes")
-                             body)
-                           sc))))
+  (doall
+    (last
+      (take (inc n) (iterate (fn [{body :body :as t}]
+                               (assert (Scope? t) "Tried to remove too many Scopes")
+                               body)
+                             sc)))))
 
 (defmulti name-to
   "Convert a name in the type to de Bruijn index of res,
@@ -135,20 +391,43 @@
     {:pre [(Type? ty)
            (symbol? name)
            (nat? res)]}
-    (:kind ty)))
+    (class ty)))
 
 (defmethod name-to B [ty name res] ty)
 
 (defmethod name-to F
-  [{name* :name :as ty} name res] 
+  [{name* :name upper :upper-bound lower :lower-bound variance :variance :as ty} name res] 
   (if (= name name*)
-    (->B res)
+    (->B res upper lower variance)
     ty))
 
+(defmethod name-to Function
+  [f name res]
+  (let [ufn #(name-to % name res)]
+    (-> f
+      (update-in [:dom] #(doall (map ufn %)))
+      (update-in [:rng] ufn)
+      (update-in [:rest] #(when %
+                            (ufn %)))
+      (update-in [:drest] #(when %
+                            (ufn %))))))
+
+(defmethod name-to Union
+  [{:keys [types]} name res]
+  (->Union (doall (map #(name-to % name res) types))))
+
 (defmethod name-to RClass
-  [{:keys [the-class replacements]} name res]
-  (->RClass the-class (into {} (for [[k v] replacements]
-                                 [k (name-to v name res)]))))
+  [{:keys [variances the-class replacements]} name res]
+  (->RClass variances
+            the-class 
+            (into {} (for [[k v] replacements]
+                       (let [v (remove-scopes (count variances) v)]
+                         [k (name-to v name (+ res (count variances)))])))))
+
+(defmethod name-to RInstance
+  [{:keys [poly? constructor]} name res]
+  (->RInstance (doall (map #(name-to % name res) poly?))
+               (name-to constructor name res)))
 
 (defmethod name-to Poly
   [{n :nbound scope :scope} name res]
@@ -158,7 +437,7 @@
 (defn- rev-indexed 
   "'(a b c) -> '([2 a] [1 b] [0 c])"
   [c]
-  (map vector (range (dec (count c)) -1 -1) c))
+  (map vector (iterate dec (dec (count c))) c))
 
 (defn abstract-many 
   "Names Type -> Scope^n  where n is (count names)"
@@ -176,18 +455,23 @@
       (add-scopes n))))
 
 (defmulti replace-image
+  "Replace all bound variables with index target, with
+  the free variable image, keeping bound variable's upper/lower bounds"
   (fn [type image target]
     {:pre [(Type? type)
            (F? image)
            (nat? target)]}
-    (:kind type)))
+    (class type)))
 
 (defmethod replace-image F [ty image target] ty)
 
 (defmethod replace-image B
-  [{idx :idx :as ty} image target]
+  [{idx :idx upper :upper-bound lower :lower-bound variance :variance :as ty} image target]
   (if (= idx target)
-    image
+    (assoc image
+           :upper-bound upper
+           :lower-bound lower
+           :variance variance)
     ty))
 
 (defmethod replace-image Poly
@@ -223,10 +507,13 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Subtypes
 
+(defn type-error [s t]
+  (throw (Exception. (str s " is not a subtype of: " t))))
+
 (def ^:dynamic *A*)
 (def ^:dynamic *A0*)
 
-(defmulti subtype* (fn [s t] [(:kind s) (:kind t)]))
+(defmulti subtype* (fn [s t] [(class s) (class t)]))
 
 (defn subtypeA* [A s t]
   (cond
@@ -242,9 +529,66 @@
 (defn subtype [s t]
   (subtypeA* #{} s t))
 
+(defmethod subtype* [RInstance RInstance]
+  [{polyl? :poly? constl :constructor :as s}
+   {polyr? :poly? constr :constructor :as t}]
+  (cond 
+    (and (= constl constr)
+         (or (not (or polyl? polyr?)) ;no type args, 
+             (let [{variances :variances} constl]
+               (every? identity
+                       (map #(case %1
+                               :covariant (subtype* %2 %3)
+                               :contravariant (subtype* %3 %2)
+                               (= %2 %3))
+                            variances
+                            polyl?
+                            polyr?)))))
+    *A0*
+
+    :else (type-error s t)))
+
 (defmethod subtype* [Type Top]
   [s t]
+  *A0*)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Altered Classes
+
+(alter-class Seqable [[a :variance :covariant]])
+
+(alter-class IPersistentCollection [[a :variance :covariant]]
+             :replace
+             {Seqable (Seqable a)})
+
+(alter-class ISeq [[a :variance :covariant]]
+             :replace
+             {Seqable (Seqable a)
+              IPersistentCollection (IPersistentCollection a)})
+
+(alter-class ILookup [[a :variance :invariant]
+                      [b :variance :covariant]])
+
+(alter-class ASeq [[a :variance :invariant]]
+             :replace
+             {IPersistentCollection (IPersistentCollection a)
+              Seqable (Seqable a)
+              ISeq (ISeq a)})
+
+(alter-class Cons [[a :variance :invariant]]
+             :replace
+             {IPersistentCollection (IPersistentCollection a)
+              ASeq (ASeq a)
+              Seqable (Seqable a)
+              ISeq (ISeq a)})
+
+(comment
+  (alter-class IPersistentCollection [a])
+  (IPersistentCollection Integer)
   )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Checker
 
 (defmulti check :op)
 
--- test/typed/test/frees.clj
+++ test/typed/test/frees.clj
@@ -1,22 +1,23 @@
 (ns typed.test.frees
+  (:refer-clojure :exclude [defrecord])
   (:use [clojure.test]
         [typed.new]))
 
 (deftest add-scopes-test
-  (is (let [body (F 'a)]
+  (is (let [body (->F 'a)]
         (= (add-scopes 0 body)
            body)))
-  (is (let [body (F 'a)]
+  (is (let [body (->F 'a)]
         (= (add-scopes 1 body)
-           (Scope body))))
-  (is (let [body (F 'a)]
+           (->Scope body))))
+  (is (let [body (->F 'a)]
         (= (add-scopes 3 body)
-           (-> body Scope Scope Scope)))))
+           (-> body ->Scope ->Scope ->Scope)))))
 
 (deftest remove-scopes-test
-  (is (let [scope (Scope (F 'a))]
+  (is (let [scope (->Scope (->F 'a))]
         (= (remove-scopes 0 scope)
            scope)))
-  (is (let [body (F 'a)]
-        (= (remove-scopes 1 (Scope body))
+  (is (let [body (->F 'a)]
+        (= (remove-scopes 1 (->Scope body))
            body))))
