a395af1860a902b7b75cb7a5860c85f8b722706a
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2013-03-13 14:10:09 +0800

    Bump Clojure version to 1.5.1

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
                  [org.clojure/clojurescript "0.0-1450"]
                  [org.clojure/tools.trace "0.7.3"
                   :exclusions [org.clojure/clojure]]
-                 [org.clojure/clojure "1.5.0"]
+                 [org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.macro "0.1.0"] ;for algo.monads
                  #_[net.intensivesystems/arrows "1.3.0"
                     :exclusions [org.clojure/clojure]] ;for testing conduit, lein test wants it here?
