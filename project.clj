eaf660174b9e0778f8cf9f747be0870c97fb5560
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2013-03-21 23:22:38 +0800

    Eliminate reflection warnings in dependencies.

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/jvm.tools.analyzer "0.3.2-20130320.151939-4"
+  :dependencies [[org.clojure/jvm.tools.analyzer "0.3.2-20130321.151954-5"
                   :exclusions [org.clojure/clojure]]
                  [org.clojure/core.contracts "0.0.3"
                   :exclusions [org.clojure/clojure]]
                  [org.clojure/math.combinatorics "0.0.2"
                   :exclusions [org.clojure/clojure]]
                  [org.clojure/clojurescript "0.0-1450"]
-                 [org.clojure/tools.trace "0.7.3"
+                 [org.clojure/tools.trace "0.7.5"
                   :exclusions [org.clojure/clojure]]
                  [org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.macro "0.1.0"] ;for algo.monads
