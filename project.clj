8400dc87eb6fdee32478ed079b0cec29f6c4f478
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2013-04-08 09:59:52 +0800

    Use jvm.tools.analyzer 0.3.5

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/jvm.tools.analyzer "0.3.5-20130331.101833-4"
+  :dependencies [[org.clojure/jvm.tools.analyzer "0.3.5"
                   :exclusions [org.clojure/clojure]]
                  [org.clojure/core.contracts "0.0.4"
                   :exclusions [org.clojure/clojure]]
