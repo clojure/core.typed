562bd0af3e2c560dbc24e32ff0fa4ef9561a4c8e
Tom Faulhaber <git_net@infolace.com>
2013-09-07 06:24:03 -0700

    Autodoc commit for master/048c9ca3

--- index-0.2.5.clj
+++ index-0.2.5.clj
@@ -20,7 +20,7 @@
    :doc
    "This namespace contains easy tools for hole driven development"}
   {:source-url
-   "https://github.com/clojure/core.typed/blob/830d22f6a7548ed7826ccb558821642a0549be04/src/main/clojure/clojure/core/typed/util_cljs.clj",
+   "https://github.com/clojure/core.typed/blob/048c9ca3dbf7218157423ddf8e18f2a12a48eb9e/src/main/clojure/clojure/core/typed/util_cljs.clj",
    :wiki-url
    "http://clojure.github.com/core.typed/clojure.core.typed.util-cljs-api.html",
    :name "clojure.core.typed.util-cljs",
@@ -1047,12 +1047,12 @@
    :name "with-core-cljs",
    :namespace "clojure.core.typed.util-cljs",
    :source-url
-   "https://github.com/clojure/core.typed/blob/830d22f6a7548ed7826ccb558821642a0549be04/src/main/clojure/clojure/core/typed/util_cljs.clj#L29",
+   "https://github.com/clojure/core.typed/blob/048c9ca3dbf7218157423ddf8e18f2a12a48eb9e/src/main/clojure/clojure/core/typed/util_cljs.clj#L12",
    :raw-source-url
-   "https://github.com/clojure/core.typed/raw/830d22f6a7548ed7826ccb558821642a0549be04/src/main/clojure/clojure/core/typed/util_cljs.clj",
+   "https://github.com/clojure/core.typed/raw/048c9ca3dbf7218157423ddf8e18f2a12a48eb9e/src/main/clojure/clojure/core/typed/util_cljs.clj",
    :wiki-url
    "http://clojure.github.com/core.typed//clojure.core.typed-api.html#clojure.core.typed.util-cljs/with-core-cljs",
    :doc "Ensure that core.cljs has been loaded.",
    :var-type "macro",
-   :line 29,
+   :line 12,
    :file "src/main/clojure/clojure/core/typed/util_cljs.clj"})}
