caedf1ab45364e54efd3fb5af2ccc5e72f1cfe6a
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2013-09-21 16:46:47 +0800

    Remove tools.macro testing dep

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject core.typed "0.2.3-SNAPSHOT"
+(defproject core.typed "0.2.9-SNAPSHOT"
   :description "Gradual typing for Clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -15,9 +15,6 @@
                   :exclusions [org.clojure/clojure]]
                  [org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.namespace "0.2.4"]
-                 [org.clojure/tools.macro "0.1.0"] ;for algo.monads
-                 #_[net.intensivesystems/arrows "1.3.0"
-                    :exclusions [org.clojure/clojure]] ;for testing conduit, lein test wants it here?
                  [com.taoensso/timbre "2.1.2"]
                  [org.clojure/core.match "0.2.0-alpha12"]
                  [org.clojure/core.async "0.1.222.0-83d0c2-alpha"]
