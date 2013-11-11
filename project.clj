18558bf0503f507588ae246a84f81d8941f1df35
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2013-11-11 19:33:02 +0800

    correct groupid in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject core.typed "0.2.18-SNAPSHOT"
+(defproject org.clojure/core.typed "0.2.18-SNAPSHOT"
   :description "Gradual typing for Clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
