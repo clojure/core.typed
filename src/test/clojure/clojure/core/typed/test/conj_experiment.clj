3c86d3d610f2a33d7ef8cf6fba63eccd1f92e509
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2013-12-06 16:35:45 +0800

    Experiment with new API

--- src/test/clojure/clojure/core/typed/test/conj_experiment.clj
+++ src/test/clojure/clojure/core/typed/test/conj_experiment.clj
@@ -1,10 +1,9 @@
 (ns clojure.core.typed.test.conj-experiment
-  (:refer-clojure :exclude [conj into])
+  (:refer-clojure :exclude [conj into with-meta])
   (:require [clojure.core.typed :as t])
   (:import (clojure.lang IMapEntry)))
 
-(comment
-
+(do
 ; # ICollection
 ;  
 ; This interface defines the extension point for `conj`, the polymorphic conjoin function.
@@ -109,104 +108,216 @@
 ;   :< (Rec [m]
 ;        (TFn [[arg :variance :covariant
 ;               :< max-arg]] 
-;          (Rec [c]
-;            (ICollection max-arg m c))))]
+;          (ICollection max-arg m)))]
 ;
 ; The upper bound constrains `res` to be a type function that takes a `conj` argument (bounded
 ; by `max-arg`) and returns an ICollection.
 
 (t/ann-protocol [[max-arg :variance :covariant]
                  [res :variance :covariant
+                  :> (TFn [[arg :variance :covariant
+                            :< max-arg]] 
+                       Nothing)
                   :< (Rec [m]
                        (TFn [[arg :variance :covariant
                               :< max-arg]] 
-                         (Rec [c]
-                           (IColl Any m c))))]]
+                         (IColl max-arg m)))
+                  ]]
                 IColl
                 -conj 
                 (All [[a :< max-arg]] 
                   [(IColl max-arg res) a a * -> (res a)])
                 )
 
+; # IEmptyableCollection
+;
+; This is part of IPersistentCollection in Clojure.
+;
+; Simply returns an empty collection of the same type as the target
+; collection. -empty just returns an IColl.
+
 (t/ann-protocol [[emp :variance :covariant
+                  :> Nothing
                   :< (Rec [m]
                        (IColl Any 
                               (TFn [[arg :variance :covariant]]
-                                m)
-                              m))]]
+                                m)))]]
                 IEmptyableCollection
-                ;empty 
-                ;[(IEmptyableCollection emp) -> emp]
+                -empty 
+                [(IEmptyableCollection emp) -> emp]
                 )
 
-(t/ann-protocol [[metafn :variance :covariant
-                  :< (Rec [m]
-                       (TFn [[new-meta :variance :covariant
-                              :< (U nil (Map Any Any))]]
-                         (I (IMeta new-meta)
-                            (IWithMeta m))))]]
-                IWithMeta
-                ;with-meta
-                ;(All [mmap]
-                ; [(IWithMeta metafn) mmap -> (metafn mmap)])
-                )
+; # IMeta
+;
+; Reading mutable or immutable metadata.
+;
+; -meta returns the current metadata of the target.
 
 (t/ann-protocol [[mmap :variance :covariant
-                  :< (U nil (Map Any Any))]]
+                  :< (U nil (t/Map Any Any))]]
                 IMeta
-                ;meta
-                ;[(IMeta mmap) -> mmap]
+
+                -meta
+                [(IMeta mmap) -> mmap]
                 )
 
+(t/defprotocol> IMeta
+  (-meta [this]))
+
+; # IWithMeta
+;
+; IObj in Clojure.
+; 
+; Writing immutable metadata.
+(t/ann-protocol [[metafn :variance :covariant
+                  :> (TFn [[new-meta :variance :covariant
+                            :< (U nil (t/Map Any Any))]]
+                          Nothing)
+                  :< (Rec [m]
+                       (TFn [[new-meta :variance :covariant
+                              :< (U nil (t/Map Any Any))]]
+                         (I (IWithMeta m)
+                            ; seems useful to want to also read metadata abstractly
+                            (IMeta new-meta))))]]
+                ;TODO the first argument should be implicit in protocols annotations
+                IWithMeta
+                -with-meta
+                (All [[mmap :< (U nil (t/Map Any Any))]]
+                 [(IWithMeta metafn) mmap -> (metafn mmap)])
+                )
+
+(t/defprotocol> IWithMeta
+  (-with-meta [this m]))
+
+(t/ann-datatype [[m :variance :covariant
+                  :< (U nil (t/Map Any Any))]]
+                Symbol
+                [name :- String
+                 meta-map :- m]
+                :unchecked-ancestors
+                [(IMeta m)
+                 (IWithMeta Symbol)])
+(deftype Symbol [name meta-map]
+  IWithMeta
+  ; (All [mmap]
+  ;   [(IWithMeta metafn) mmap -> (metafn mmap)])
+  (-with-meta [this m]
+    (t/inst-ctor (Symbol. name m) mmap))
+
+
+  ; TODO in general we should use the protocol method's annotated type to
+  ; check any implementations of it. Interesting cases exist if the protocol
+  ; is parameterised: here IMeta is (IMeta m) as provided in :unchecked-ancestors
+  ; so we should check the implementation of -meta (below) against the type of -meta
+  ; but with the first type parameter of IMeta instantiated to `m`.
+  IMeta
+  (-meta [this] meta-map))
+
+(t/ann ^:no-check with-meta 
+       (All [[metafn
+              :> (TFn [[new-meta :variance :covariant
+                        :< (U nil (t/Map Any Any))]]
+                      Nothing)
+              :< (Rec [m]
+                      (TFn [[new-meta :variance :covariant
+                             :< (U nil (t/Map Any Any))]]
+                           (I (IWithMeta m)
+                              (IMeta new-meta))))]
+             [m :< (U nil (t/Map Any Any))]]
+          [(IWithMeta metafn) m -> (metafn m)]))
+(defn with-meta [a m]
+  (-with-meta a m))
+
+
+; # ISeqable
+;
+; Immutable representation of the target collection.
+;
+; -seq returns the nonempty persistent collection or nil. Its
+; return type allows the target collection full flexibility for
+; the type of collection it returns.
 (t/ann-protocol [[s :variance :covariant
-                  :< (U nil (NonEmptySeq Any))]]
+                  :< (U nil (t/NonEmptySeq Any))]]
                 ISeqable
-                ;seq
-                ;[(ISeqable s) -> s]
+
+                -seq
+                [(ISeqable s) -> s]
                 )
 
-(t/ann-protocol [[s :variance :covariant]]
+; update metadata
+; update map entries
+; update seqable length
+; update 
+
+; Who should take the brunt of the work for updating types?
+;
+; We could rely on predicates like every? to generate better types.
+; eg. (every? number? myvec) could infer (is (Vec Number) myvec) rather than
+;     (is (Seqable Number) myvec). This would be simple to update myvec with.
+;
+; Or we could make the intersection simplification/combination process
+; smarter or more extensible.
+;
+; eg. combining (is (Vec Any) myvec) and (is (Seqable Number) myvec) somehow
+;     infers (is (Vec Number) myvec)
+
+; # ISeq
+(t/ann-protocol [[s :variance :covariant]
+                 [l :variance :covariant
+                  :< (CountRange 0)]]
                 ISeq
-                ;first
-                ;[(ISeq s) -> (U nil s)]
-                ;rest
-                ;[(ISeq s) -> (Seq s)]
+                -first
+                (Fn [(ISeq s (CountRange 1)) -> s]
+                    [(ISeq s l) -> (U nil s)])
+                -rest
+                [(ISeq s l) -> (ISeq s (CountRange 0))]
                 )
 
 (t/ann-protocol [[s :variance :covariant]]
                 INext
-                ;next
-                ;[(INext s) -> (U nil (Seq x))]
+                -next
+                [(INext s) -> (U nil (t/NonEmptySeq s))]
                 )
 
-(t/ann first (All [x]
-               (Fn [(ISeqable (NonEmptySeq x)) -> x]
-                   [(U nil (ISeqable (U nil (NonEmptySeq x)))) -> (U nil x)])))
-
 (t/defprotocol> IColl)
-(t/defprotocol> IWithMeta)
-(t/defprotocol> IMeta)
+(t/defprotocol> ISeqable)
+(t/defprotocol> IEmptyableCollection)
+(t/defprotocol> ISeq)
+(t/defprotocol> INext)
+
+(t/ann every?
+     (All [x [y :< x] p]
+       [[y -> Any
+         :filters {:then (is p 0)}]
+        -> (U nil (ISeqable (ISeq x)))]))
+
+(t/ann first (All [x]
+               (Fn [(ISeqable (t/NonEmptySeq x)) -> x]
+                   [(U nil (ISeqable (U nil (t/NonEmptySeq x)))) -> (U nil x)])))
+
+; A type function takes a number of types and returns a type
+;
+; A function takes a number of values and returns a value
+;
+; A function intersection describes 
+; 
+; (TFn 
+;   [
 
 (t/def-alias MapLike
   (TFn [[k :variance :covariant]
         [v :variance :covariant]
         [meta :variance :covariant]]
     (I (IColl (U nil '[Any Any] (IMapEntry Any Any))
+              ; this bound probably needs to be manually instantiated 
               (All [a b]
                 (TFn 
                   [[x :variance :covariant
                     :< (U nil '[a b] (IMapEntry a b))]]
-                  (MapLike (U k a) (U v b) meta)))
-              (MapLike Nothing Nothing nil))
-       (IMeta meta
-              ; if we want to update this type with (IMeta a),
-              ; we can pass `a` to this TFn to perform this update.
-              ; Extensible type updating!
-              :update
-              (TFn [[new-meta :variance :covariant]]
-                (MapLike k v new-meta)))
+                  (MapLike (U k a) (U v b) meta))))
+       (IMeta meta)
        (IWithMeta (TFn [[new-meta :variance :covariant
-                         :< (U nil (Map Any Any))]]
+                         :< (U nil (t/Map Any Any))]]
                     (MapLike k v new-meta))))))
 
 ;; How can we make occurrence typing updating extensible?
@@ -222,20 +333,45 @@
            [a :< max-arg]
            [res :< (Rec [m]
                      (TFn [[x :variance :covariant]]
-                       (Rec [c]
-                         (IColl max-arg m c))))]]
-          (Fn [(Rec [m] (IColl max-arg res m)) a a * -> (res a)])))
+                       (IColl max-arg m)))]]
+          (Fn [(IColl max-arg res) a a * -> (res a)])))
 (defn conj [target arg & args]
   (apply clojure.core/conj target arg args))
 
-(fn []
+#_(t/cf 
+  (fn []
   (t/inst conj 
           (U nil '[Any Any] (IMapEntry Any Any))
           (U nil '[Number Number] (IMapEntry Number Number))
-          Any
-          #_(TFn [[x :variance :covariant
+          (TFn [[x :variance :covariant
                  :< (U nil '[Number Number] (IMapEntry Number Number))]]
-             (MapLike Number Number))))
+             (MapLike Number Number (U nil (t/Map Any Any)))))))
+
+; the above appears to choke on computing the following subtyping relationship:
+
+;(All
+;  [Poly-fresh-sym98115 Poly-fresh-sym98117]
+;  (TFn
+;    [[fresh-sym98258
+;      :variance
+;      :covariant
+;      :<
+;      (U
+;       (clojure.lang.IMapEntry Poly-fresh-sym98115 Poly-fresh-sym98117)
+;       nil
+;       (HVec [Poly-fresh-sym98115 Poly-fresh-sym98117]))]]
+;    (clojure.core.typed.test.conj-experiment/MapLike
+;      (U Poly-fresh-sym98115 java.lang.Number)
+;      (U java.lang.Number Poly-fresh-sym98117)
+;      (U (clojure.core.typed/Map Any Any) nil))))
+;(Rec
+;  [G__98082]
+;  (TFn
+;    [[fresh-sym98261 :variance :covariant]]
+;    (clojure.core.typed.test.conj-experiment/IColl
+;      (U (HVec [Any Any]) nil (clojure.lang.IMapEntry Any Any))
+;      G__98082)))
+
 
 ;(ann ^:no-check
 ;     into
@@ -285,3 +421,27 @@
 ;       (ILookup a b)
 ;       (Associative a b))))
 )
+
+;; WithMeta
+;; 
+;; Updating metadata
+;;
+;; Metadata seems like it might be useful to update with Assoc,
+;; but it's a "special" abstraction in Clojure that is backed by
+;; a "hidden" field __meta or _meta.
+;;
+;; (WithMeta [m]
+;;   (PersistentHashMap x y z)
+;;   (PersistentHashMap x y m))
+;;
+;; (add-WithMeta-case 
+;;   [x]
+;;   [] 
+;;   (Symbol (U nil (Map Any Any)))
+;;   (Symbol x))
+;;
+;; (add-WithMeta-case 
+;;   [m]
+;;   [k v]
+;;   (IPersistentMap k v (U nil (Map Any Any)))
+;;   (IPersistentMap k v m))
