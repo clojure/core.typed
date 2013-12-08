fdebae84391a230ada41a2d237644930f08ea318
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2013-12-09 01:00:20 +0800

    Debugging reports

--- src/main/clojure/clojure/core/typed/check.clj
+++ src/main/clojure/clojure/core/typed/check.clj
@@ -23,6 +23,8 @@
             [clojure.core.typed.subst :as subst]
             [clojure.core.typed.frees :as frees]
             [clojure.core.typed.free-ops :as free-ops]
+            [clojure.core.typed.tvar-env :as tvar-env]
+            [clojure.core.typed.tvar-bnds :as tvar-bnds]
             [clojure.core.typed.dvar-env :as dvar-env]
             [clojure.core.typed.var-env :as var-env]
             [clojure.core.typed.protocol-env :as ptl-env]
@@ -1872,9 +1874,27 @@
   ([e]
    {:pre [(lex/PropEnv? e)]}
    ;; DO NOT REMOVE
-   (prn {:env (into {} (for [[k v] (:l e)]
-                         [k (prs/unparse-type v)]))
-         :props (map prs/unparse-filter (:props e))})))
+   (let [tvar-scope tvar-env/*current-tvars*
+         tvar-bounds tvar-bnds/*current-tvar-bnds*
+         scoped-names (keys tvar-scope)
+         actual-names (map :name (vals tvar-scope))
+         _ (every? symbol? actual-names)
+         actual-bnds (map tvar-bounds actual-names)]
+     (prn {:env (into {} (for [[k v] (:l e)]
+                           [k (prs/unparse-type v)]))
+           :props (map prs/unparse-filter (:props e))
+           ;:frees (map (t/fn> 
+           ;              [nme :- Symbol, bnd :- (U nil Bounds)]
+           ;              {:pre [(symbol? nme)
+           ;                     ((some-fn nil? r/Bounds?) bnd)]}
+           ;              (if bnd
+           ;                (prs/unparse-poly-bounds-entry nme bnd)
+           ;                [nme 'NO-BOUNDS]))
+           ;            scoped-names
+           ;            actual-bnds)
+           ;:tvar-scope tvar-scope
+           ;:tvar-bnds tvar-bounds
+           }))))
 
 ;debug printing
 (add-invoke-special-method 'clojure.core.typed/print-env
