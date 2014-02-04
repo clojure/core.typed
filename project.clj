3b539c760b21c245f0d0ca246c76d0f5d9a80a47
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2014-02-04 14:35:38 +0800

    bump clojurescript in project.clj

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                   :exclusions [org.clojure/clojure]]
                  [org.clojure/math.combinatorics "0.0.2"
                   :exclusions [org.clojure/clojure]]
-                 [org.clojure/clojurescript "0.0-1859"]
+                 [org.clojure/clojurescript "0.0-2138"]
                  [org.clojure/tools.trace "0.7.5"
                   :exclusions [org.clojure/clojure]]
                  [org.clojure/clojure "1.5.1"]
