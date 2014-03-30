d702f903bacdb77afb1ec865581e70b1e22a29b5
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2014-03-30 19:17:08 +0800

    Workarounds for CLJ-979

--- src/test/clojure/clojure/core/typed/test/munge_record_field.clj
+++ src/test/clojure/clojure/core/typed/test/munge_record_field.clj
@@ -3,13 +3,20 @@
 
 (ann-datatype FooDT [normal :- Number
                      other-keys? :- Number])
-(do (deftype FooDT [normal other-keys?])
-    (ann-form (.normal ^FooDT (->FooDT 2 1)) Number))
+(deftype FooDT [normal other-keys?])
+
+; don't eval this; see CLJ-979
+(fn []
+  (ann-form (.normal ^FooDT (->FooDT 2 1)) Number)
+  )
 
 (ann-record FooRec [other-keys? :- Number])
 (defrecord FooRec [other-keys?])
 
-(ann-form (.other-keys? ^FooDT (->FooDT 2 1)) Number)
+; don't eval this; see CLJ-979
+(fn []
+  (ann-form (.other-keys? ^FooDT (->FooDT 2 1)) Number)
 
-(ann-form (:other-keys? (->FooRec 1)) Number)
-(ann-form (.other-keys? ^FooRec (->FooRec 1)) Number)
+  (ann-form (:other-keys? (->FooRec 1)) Number)
+  (ann-form (.other-keys? ^FooRec (->FooRec 1)) Number)
+  )
