98bd863a82d5308269273dd3fc84679903508b28
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2014-04-02 22:14:02 +0800

    bump project.clj version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojure/core.typed "0.2.35-SNAPSHOT"
+(defproject org.clojure/core.typed "0.2.42-SNAPSHOT"
   :description "Gradual typing for Clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
