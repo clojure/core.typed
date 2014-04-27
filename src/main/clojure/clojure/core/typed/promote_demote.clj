30a360b11d327253485bb24db90a8f2db14824e1
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2014-04-27 21:15:46 +0800

    Add promote/demote cases for HSequential

--- src/main/clojure/clojure/core/typed/promote_demote.clj
+++ src/main/clojure/clojure/core/typed/promote_demote.clj
@@ -15,7 +15,7 @@
                                         CountRange Name Value Top TopFunction B F Result AnyValue
                                         HeterogeneousSeq TCError Extends JSNominal
                                         StringCLJS BooleanCLJS NumberCLJS IntegerCLJS ObjectCLJS
-                                        ArrayCLJS FunctionCLJS KwArgsSeq)
+                                        ArrayCLJS FunctionCLJS KwArgsSeq HSequential)
            (clojure.core.typed.filter_rep TopFilter BotFilter TypeFilter NotTypeFilter AndFilter OrFilter
                                           ImpFilter)
            (clojure.core.typed.object_rep NoObject EmptyObject Path)
@@ -135,6 +135,60 @@
   [T V]
   (-> T
     (update-in [:types] handle-kw-map demote V)))
+
+(defmethod promote HSequential
+  [T V]
+  (let [pmt #(promote % V)
+        latent-filter-vs (set/intersection (set (mapcat frees/fv (:fs T)))
+                                           (set (mapcat frees/fi (:fs T))))]
+    (cond
+      ;if filter contains V, give up
+      (seq (set/intersection V latent-filter-vs)) (c/In (c/RClass-of clojure.lang.Sequential)
+                                                        (c/RClass-of clojure.lang.IPersistentCollection [r/-any]))
+
+      ;if dotted bound is in V, transfer to rest args
+      (and (:drest T) (V (-> T :drest :name)))
+      (r/-hsequential (mapv pmt (:types T))
+               :filters (:fs T)
+               :objects (:objects T)
+               :rest (pmt (-> T :drest :pre-type)))
+
+      :else
+      (r/-hsequential (mapv pmt (:types T))
+               ; we know no filters contain V
+               :filters (:fs T)
+               :objects (:objects T)
+               :rest (when-let [rest (:rest T)]
+                       (pmt rest))
+               :drest (when-let [drest (:drest T)]
+                        (update-in drest [:pre-type] pmt))))))
+
+(defmethod demote HSequential
+  [T V]
+  (let [dmt #(promote % V)
+        latent-filter-vs (set/intersection (set (mapcat frees/fv (:fs T)))
+                                           (set (mapcat frees/fi (:fs T))))]
+    (cond
+      ;if filter contains V, give up
+      (seq (set/intersection V latent-filter-vs)) (c/In (c/RClass-of clojure.lang.Sequential)
+                                                        (c/RClass-of clojure.lang.IPersistentCollection [r/-any]))
+
+      ;if dotted bound is in V, transfer to rest args
+      (and (:drest T) (V (-> T :drest :name)))
+      (r/-hsequential (mapv dmt (:types T))
+               :filters (:fs T)
+               :objects (:objects T)
+               :rest (dmt (-> T :drest :pre-type)))
+
+      :else
+      (r/-hsequential (mapv dmt (:types T))
+               ; we know no filters contain V
+               :filters (:fs T)
+               :objects (:objects T)
+               :rest (when-let [rest (:rest T)]
+                       (dmt rest))
+               :drest (when-let [drest (:drest T)]
+                        (update-in drest [:pre-type] dmt))))))
 
 (defmethod promote HeterogeneousVector
   [T V]
