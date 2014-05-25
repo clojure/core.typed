81e2b250221b21c32809133288d6c7854bba733d
Jake Goulding <jake.goulding@gmail.com>
2014-05-25 11:20:56 -0400

    Extract nth helper functions for filters
    The comments became redundant between the implementation and function
    names.
    
    Signed-off-by: Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>

--- src/main/clojure/clojure/core/typed/check/nth.clj
+++ src/main/clojure/clojure/core/typed/check/nth.clj
@@ -1,13 +1,52 @@
 (ns clojure.core.typed.check.nth
   (:require [clojure.core.typed.type-ctors :as c]
             [clojure.core.typed.type-rep :as r]
+            [clojure.core.typed.object-rep :as obj]
             [clojure.core.typed.utils :as u]
+            [clojure.core.typed.contract-utils :as con]
             [clojure.core.typed.errors :as err]
             [clojure.core.typed.parse-unparse :as prs]
             [clojure.core.typed.filter-ops :as fo]
             [clojure.core.typed.filter-rep :as fl]
             [clojure.core.typed.check.utils :as cu])
   (:import (clojure.lang ISeq Seqable)))
+
+(defn ^:private nth-positive-filter-default-truthy
+  [target-o default-o]
+  {:pre [(obj/RObject? target-o)
+         (obj/RObject? default-o)]
+   :post [(fl/Filter? %)]}
+  (fo/-and (fo/-filter-at (c/Un r/-nil (c/RClass-of ISeq [r/-any]))
+                          target-o)
+           (fo/-not-filter-at (c/Un r/-false r/-nil)
+                              default-o)))
+
+(defn ^:private nth-positive-filter-default-falsy [target-o default-o nnth]
+  {:pre [(obj/RObject? target-o)
+         (obj/RObject? default-o)
+         (con/nat? nnth)]
+   :post [(fl/Filter? %)]}
+  (fo/-and (fo/-filter-at (c/In (c/RClass-of Seqable [r/-any])
+                                (r/make-CountRange (inc nnth)))
+                          target-o)
+           (fo/-filter-at (c/Un r/-false r/-nil)
+                          default-o)))
+
+(defn ^:private nth-positive-filter-default [target-o default-o nnth]
+  {:pre [(obj/RObject? target-o)
+         (obj/RObject? default-o)
+         (con/nat? nnth)]
+   :post [(fl/Filter? %)]}
+  (fo/-or (nth-positive-filter-default-truthy target-o default-o)
+          (nth-positive-filter-default-falsy target-o default-o nnth)))
+
+(defn ^:private nth-positive-filter-no-default [target-o nnth]
+  {:pre [(obj/RObject? target-o)
+         (con/nat? nnth)]
+   :post [(fl/Filter? %)]}
+  (fo/-filter-at (c/In (c/RClass-of Seqable [r/-any])
+                       (r/make-CountRange (inc nnth)))
+                 target-o))
 
 (defn invoke-nth [check-fn {:keys [args] :as expr} expected & {:keys [cargs]}]
   {:pre [((some-fn nil? vector?) cargs)]}
@@ -44,31 +83,11 @@
                                   target-o (r/ret-o (u/expr-type te))
                                   default-o (when de
                                               (r/ret-o (u/expr-type de)))
-                                  ;; We handle filters for both arities of nth here, with and without default
-                                  ;;
-                                  ;;With default:
-                                  ;; if this is a true value either:
-                                  ;;  * target is nil or seq and default is true
-                                  ;;  * target is seqable, default is false
-                                  ;;    and target is at least (inc nnth) count
-                                  default-fs+ (fo/-or (fo/-and (fo/-filter-at (c/Un r/-nil (c/RClass-of ISeq [r/-any])) 
-                                                                              target-o)
-                                                               (fo/-not-filter-at (c/Un r/-false r/-nil) 
-                                                                                  default-o))
-                                                      (fo/-and (fo/-filter-at (c/In (c/RClass-of Seqable [r/-any])
-                                                                                    (r/make-CountRange (inc nnth)))
-                                                                              target-o)
-                                                               (fo/-filter-at (c/Un r/-false r/-nil) 
-                                                                              default-o)))
-                                  ;;Without default:
-                                  ;; if this is a true value: 
-                                  ;;  * target is seqable of at least nnth count
-                                  nodefault-fs+ (fo/-filter-at (c/In (c/RClass-of Seqable [r/-any])
-                                                                     (r/make-CountRange (inc nnth)))
-                                                               target-o)]
-                              (fo/-FS (if default-t
-                                        default-fs+
-                                        nodefault-fs+)
+
+                                  filter+ (if default-t
+                                            (nth-positive-filter-default target-o default-o nnth)
+                                            (nth-positive-filter-no-default target-o nnth))]
+                              (fo/-FS filter+
                                       ; not sure if there's anything worth encoding here
                                       fl/-top))))
       :else cu/not-special)))
