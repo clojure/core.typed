cb284eb11f56c85cd231b6057005d67e5f92e9c5
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2014-05-31 00:37:11 +0800

    Move special fn op

--- src/main/clojure/clojure/core/typed/check.clj
+++ src/main/clojure/clojure/core/typed/check.clj
@@ -42,6 +42,7 @@
             [clojure.core.typed.check.utils :as cu]
             [clojure.core.typed.check.value :as value]
             [clojure.core.typed.check.special.ann-form :as ann-form]
+            [clojure.core.typed.check.special.fn :as special-fn]
             [clojure.core.typed.coerce-utils :as coerce]
             [clojure.core.typed.contract-utils :as con]
             [clojure.core.typed.special-form :as spec]
@@ -1325,39 +1326,7 @@
 
 (defmethod internal-special-form ::t/fn
   [{[_ _ {{fn-anns :ann} :val} :as statements] :statements fexpr :ret :as expr} expected]
-  {:pre [(#{3} (count statements))]}
-  (let [ann-expected
-        (binding [prs/*parse-type-in-ns* (cu/expr-ns expr)]
-          (apply
-            r/make-FnIntersection
-            (doall
-              (for [{:keys [dom rest drest ret-type]} fn-anns]
-                (r/make-Function (mapv (comp prs/parse-type :type) dom)
-                                 (prs/parse-type (:type ret-type))
-                                 (when rest
-                                   (prs/parse-type (:type rest)))
-                                 (when drest
-                                   (r/DottedPretype1-maker
-                                     (prs/parse-type (:pretype drest))
-                                     (:bound drest))))))))
-
-        ; if the t/fn statement looks unannotated, use the expected type if possible
-        use-expected (if (every? (fn [{:keys [dom rest drest rng] :as f}]
-                                   {:pre [(r/Function? f)]}
-                                   (and (every? #{r/-any} dom)
-                                        ((some-fn nil? #{r/-any}) rest)
-                                        (#{r/-any} (:t rng))))
-                                 (:types ann-expected))
-                       (or (when expected (r/ret-t expected)) ann-expected)
-                       ann-expected)
-        cfexpr (check fexpr (r/ret use-expected))
-        _ (when expected
-            (let [actual (-> cfexpr u/expr-type r/ret-t)]
-              (when-not (sub/subtype? actual (r/ret-t expected))
-                (cu/expected-error actual (r/ret-t expected)))))]
-    (assoc expr
-           :ret cfexpr
-           u/expr-type (u/expr-type cfexpr))))
+  (special-fn/check-special-fn check expr expected))
 
 (defmethod internal-special-form ::t/ann-form
   [{[_ _ {{tsyn :type} :val} :as statements] :statements frm :ret, :keys [env], :as expr} expected]
