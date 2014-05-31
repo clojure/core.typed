53d444335dfc055dd7f8ae73c2dd3336bd511df5
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2014-06-01 00:43:00 +0800

    Move gen-protocol

--- src/main/clojure/clojure/core/typed/collect_phase.clj
+++ src/main/clojure/clojure/core/typed/collect_phase.clj
@@ -2,6 +2,7 @@
   (:require [clojure.core.typed :as t]
             [clojure.core.typed.collect.typed-deps :as typed-deps]
             [clojure.core.typed.collect.gen-datatype :as gen-datatype]
+            [clojure.core.typed.collect.gen-protocol :as gen-protocol]
             [clojure.core.typed.type-rep :as r]
             [clojure.core.typed.type-ctors :as c]
             [clojure.core.typed.utils :as u]
@@ -318,93 +319,19 @@
     (binding [prs/*parse-type-in-ns* current-ns]
       (prs/parse-free-binder-with-variance binder))))
 
-(defn gen-protocol* [current-env current-ns vsym binder mths]
-  {:pre [(symbol? current-ns)]}
-  (let [_ (when-not (symbol? vsym)
-            (err/int-error
-              (str "First argument to ann-protocol must be a symbol: " vsym)))
-        s (if (namespace vsym)
-            (symbol vsym)
-            (symbol (str current-ns) (name vsym)))
-        ;_ (prn "gen-protocol*" s)
-        protocol-defined-in-nstr (namespace s)
-        on-class (c/Protocol-var->on-class s)
-        ; add a Name so the methods can be parsed
-        _ (nme-env/declare-protocol* s)
-        ;_ (prn "gen-protocol before parsed-binder")
-        parsed-binder (when binder 
-                        (binding [prs/*parse-type-in-ns* current-ns]
-                          (prs/parse-free-binder-with-variance binder)))
-        ;_ (prn "gen-protocol after parsed-binder")
-        fs (when parsed-binder
-             (map (comp r/make-F :fname) parsed-binder))
-        bnds (when parsed-binder
-               (map :bnd parsed-binder))
-        _ (assert (= (count fs) (count bnds)))
-        _ (assert ((some-fn nil? map?) mths))
-        _ (when-let [[m] (seq (remove symbol? (keys mths)))]
-            (err/int-error (str "Method names to ann-protocol must be symbols: " m)))
-        _ (doseq [[n1 n2] (comb/combinations (keys mths) 2)]
-            (when (= (munge n1) (munge n2))
-              (err/int-error 
-                (str "Protocol methods for " vsym " must have distinct representations: "
-                     "both " n1 " and " n2 " compile to " (munge n1)))))
-        ms (into {} (for [[knq v] mths]
-                      (let [_ (when (namespace knq)
-                                (err/int-error "Protocol method should be unqualified"))
-                            mtype (free-ops/with-bounded-frees (zipmap fs bnds)
-                                    (binding [vs/*current-env* current-env
-                                              prs/*parse-type-in-ns* current-ns]
-                                      (prs/parse-type v)))]
-                         (let [rt (c/fully-resolve-type mtype)
-                               fin? (fn [f]
-                                      (let [f (c/fully-resolve-type f)]
-                                        (boolean
-                                          (when (r/FnIntersection? f)
-                                            (every? seq (map :dom (:types f)))))))]
-                           (when-not 
-                             (or
-                               (fin? rt)
-                               (when (r/Poly? rt) 
-                                 (let [names (c/Poly-fresh-symbols* rt)]
-                                   (fin? (c/Poly-body* names rt))))
-                               (when (r/PolyDots? rt) 
-                                 (let [names (c/PolyDots-fresh-symbols* rt)]
-                                   (fin? (c/PolyDots-body* names rt)))))
-                             (err/int-error (str "Protocol method " knq " should be a possibly-polymorphic function intersection"
-                                               " taking at least one fixed argument: "
-                                               (prs/unparse-type mtype)))))
-                         [knq mtype])))
-        ;_ (prn "collect protocol methods" (into {} ms))
-        t (c/Protocol* (map :name fs) (map :variance parsed-binder) 
-                       fs s on-class ms (map :bnd parsed-binder))]
-    (ptl-env/add-protocol s t)
-    ; annotate protocol var as Any
-    (var-env/add-nocheck-var s)
-    (var-env/add-var-type s r/-any)
-    (doseq [[kuq mt] ms]
-      (assert (not (namespace kuq))
-              "Protocol method names should be unqualified")
-      ;qualify method names when adding methods as vars
-      (let [kq (symbol protocol-defined-in-nstr (name kuq))
-            mt-ann (clt-u/protocol-method-var-ann mt (map :name fs) bnds)]
-        (var-env/add-nocheck-var kq)
-        (var-env/add-var-type kq mt-ann)))
-    ;(prn "end gen-protocol" s)
-    nil))
 
 (defmethod invoke-special-collect 'clojure.core.typed/ann-protocol*
   [{:keys [args env] :as expr}]
   (clt-u/assert-expr-args expr #{3})
   (let [[binder varsym mth] (ast-u/constant-exprs args)]
-    (gen-protocol* env (chk-u/expr-ns expr) varsym binder mth)))
+    (gen-protocol/gen-protocol* env (chk-u/expr-ns expr) varsym binder mth)))
 
 (defmethod invoke-special-collect 'clojure.core.typed/ann-pprotocol*
   [{:keys [args env] :as expr}]
   (clt-u/assert-expr-args expr #{3})
   (let [[varsym binder mth] (ast-u/constant-exprs args)]
     (assert nil "UNSUPPPORTED OPERATION: ann-pprotocol, use ann-protocol with binder as first argument, ie. before protocol name")
-    #_(gen-protocol* env (chk-u/expr-ns expr) varsym binder mth)))
+    #_(gen-protocol/gen-protocol* env (chk-u/expr-ns expr) varsym binder mth)))
 
 
 (defmethod invoke-special-collect :default
