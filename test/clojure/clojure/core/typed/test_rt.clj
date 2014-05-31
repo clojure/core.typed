6aed5e11d40e3c1507032096b50d8f4297bc91fb
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2014-05-31 14:56:55 +0800

    Include CLJS rt files in core.typed.rt

--- test/clojure/clojure/core/typed/test_rt.clj
+++ test/clojure/clojure/core/typed/test_rt.clj
@@ -1,5 +1,6 @@
 (ns clojure.core.typed.test-rt
   (:require [clojure.core.typed :as t]
+            [cljs.core.typed :as tcljs]
             [clojure.core.typed.errors :as err]
             [clojure.java.io :as io])
   (:use clojure.test))
@@ -15,6 +16,14 @@
 
 (deftest checking-ops
   (is (err/tc-error-thrown?
+        (t/load-if-needed)))
+  (is (err/tc-error-thrown?
+        (t/reset-caches)))
+  (is (err/tc-error-thrown?
+        (t/method-type 'foo)))
+  (is (err/tc-error-thrown?
+        (t/into-array> 'foo 'bar [1])))
+  (is (err/tc-error-thrown?
         (t/cf 1)))
   (is (err/tc-error-thrown?
         (t/check-form* 1)))
@@ -28,3 +37,22 @@
         (t/statistics ['foo])))
   (is (err/tc-error-thrown?
         (t/var-coverage))))
+
+(deftest checking-cljs-ops
+  (is (err/tc-error-thrown?
+        (tcljs/load-if-needed)))
+  (is (err/tc-error-thrown?
+        (tcljs/reset-caches)))
+  (is (err/tc-error-thrown?
+        (tcljs/cf* 1 nil nil)))
+  (is (err/tc-error-thrown?
+        (tcljs/cf 1)))
+  (is (err/tc-error-thrown?
+        (tcljs/check-ns*)))
+  (is (err/tc-error-thrown?
+        (tcljs/check-ns* 'foo)))
+  ; these throw at macroexpansion time
+  (is (err/tc-error-thrown?
+        (eval '(cljs.core.typed/check-ns))))
+  (is (err/tc-error-thrown?
+        (eval '(cljs.core.typed/check-ns foo)))))
