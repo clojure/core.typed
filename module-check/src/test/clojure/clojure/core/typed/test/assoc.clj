e74a533e61cd9e9431d4e16f7b793ed3a50237d9
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2014-07-20 17:46:57 +0800

    Fix CTYP-161: Support intersections of HMap's as first argument of assoc

--- module-check/src/test/clojure/clojure/core/typed/test/assoc.clj
+++ module-check/src/test/clojure/clojure/core/typed/test/assoc.clj
@@ -1,19 +1,30 @@
 (ns clojure.core.typed.test.assoc
-  (:require [clojure.core.typed :refer [ann-form check-ns] :as t])
+  (:require 
+    [clojure.core.typed.test.test-utils :refer :all]
+    [clojure.core.typed :refer [ann-form check-ns] :as t]
+    [clojure.test :refer :all])
   (:import (clojure.lang Symbol)))
 
-(ann-form (assoc {:a 1} :b 2)
-          '{:a Number :b Number})
+(deftest assoc-test
+  (is-tc-e (assoc {:a 1} :b 2)
+           '{:a Number :b Number})
 
-#(let [a 1]
-   (assoc a :b 2))
+  (is-tc-err (let [a 1]
+               (assoc a :b 2)))
+  (is-tc-err
+    (do (t/ann-record FooRec [a :- t/Num
+                              b :- t/Symbol])
+        (defrecord FooRec [a b])
+        (assoc (->FooRec 1 'a) :a 'b)))
 
-(t/ann-record FooRec [a :- Number
-                      b :- Symbol])
-(defrecord FooRec [a b])
+  (is-tc-e
+    (do (t/ann-record FooRec [a :- t/Num
+                              b :- t/Symbol])
+        (defrecord FooRec [a b])
+        (assoc (->FooRec 1 'a) :a 4)))
 
-#(assoc (->FooRec 1 'a) :a 'b)
-
-#(assoc (->FooRec 1 'a) :a 4)
-
-#(update-in (->FooRec 1 'a) [:a] (constantly 2))
+  ;intersections
+  (is-tc-e (t/fn [m :- (t/I (t/HMap :mandatory {:foo Num})
+                            (t/HMap :mandatory {:bar Num}))]
+             :- '{:foo Num, :bar Num, :baz Num}
+             (assoc m :baz 2))))
