468634c493f0f10200f6a928957d590e9ce5275b
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2014-07-21 13:44:51 +0800

    Add all-envs file

---
+++ src/main/clojure/clojure/core/typed/all_envs.clj
@@ -0,0 +1,28 @@
+(ns ^:skip-wiki clojure.core.typed.all-envs
+  (:require [clojure.core.typed.name-env :as nme-env]
+            [clojure.core.typed.parse-unparse :as prs]
+            [clojure.core.typed.util-vars :as vs]
+            [clojure.core.typed.var-env :as var-env]
+            [clojure.core.typed.current-impl :as impl]
+            ))
+
+(defn name-env []
+  (let [nme-env (nme-env/name-env)]
+    (binding [vs/*verbose-types* true]
+      (into {}
+            (for [[k v] nme-env]
+              (when-not (keyword? v)
+                [k (prs/unparse-type v)]))))))
+
+(defn var-env []
+  (let [var-env (var-env/var-annotations)]
+    (assert var-env)
+    (binding [vs/*verbose-types* true]
+      (into {}
+            (for [[k v] var-env]
+              [k (prs/unparse-type v)])))))
+
+(defn all-envs-clj []
+  (impl/with-clojure-impl
+    {:aliases (name-env)
+     :vars (var-env)}))
