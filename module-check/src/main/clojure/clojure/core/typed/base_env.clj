c6ad6404274dfbfa297223808679fff6d2fea204
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2014-07-28 12:39:23 +0200

    Enhancing clojure.string/blank? to support nil parameter

--- module-check/src/main/clojure/clojure/core/typed/base_env.clj
+++ module-check/src/main/clojure/clojure/core/typed/base_env.clj
@@ -721,7 +721,7 @@ clojure.string/join
 clojure.string/upper-case
       [CharSequence -> String]
 
-clojure.string/blank? [String -> Boolean]
+clojure.string/blank? [(U nil String) -> Boolean]
 clojure.string/capitalize [String -> String]
 clojure.string/lower-case [String -> String]
 clojure.string/replace (IFn [String String String -> String]  [String Character Character -> String]  [String java.util.regex.Pattern (U String [String -> String]) -> String] )
