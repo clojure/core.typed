42f6aca4df7b023ec9b6a33d53c32d241e1a278c
ympbyc <ympbyc@gmail.com>
2014-08-03 08:29:41 -0700

    add more tests. Known issues: subtyping for HSequential is incomplete.  promote is not defined for HSet

--- module-check/src/test/clojure/clojure/core/typed/test/cljs_core.clj
+++ module-check/src/test/clojure/clojure/core/typed/test/cljs_core.clj
@@ -1,7 +1,7 @@
 (ns clojure.core.typed.test.cljs-core
-	(:require [cljs.core.typed :as t]
+    (:require [cljs.core.typed :as t]
               [cljs.core :as core]
-              [clojure.core.typed.test.cljs-utils :refer [is-tc-e]]
+              [clojure.core.typed.test.cljs-utils :refer [is-tc-e tc-e]]
               [clojure.test :refer :all]))
 
 ;;; defining tests this way should help estimate the coverage
@@ -12,126 +12,216 @@
 (defmacro add-test [f & pairs]
   (assert (even? (count pairs)))
   `(swap! core-tests conj 
-  	['~(symbol "cljs.core" (name f)) '~pairs]))
+      ['~(symbol "cljs.core" (name f)) '~pairs]))
 
 ;;tests for single parameter functions
 (defmacro add-test1 [f & pairs]
   (assert (even? (count pairs)))
   `(swap! core-tests conj
-  	[(with-meta '~(symbol "cljs.core" (name f)) 
-  	  {:single-arg true})
-  	 '~pairs]))
+      [(with-meta '~(symbol "cljs.core" (name f)) 
+        {:single-arg true})
+       '~pairs]))
 
 (defmacro run-core-tests []
   (cons 'do 
-  	(for [[f cases] @core-tests
-  	      [args expected] (partition 2 cases)]
-  	  (if (:single-arg (meta f))
-  	  	`(is-tc-e (~f ~args) ~expected)
-  	  	`(is-tc-e (~f ~@args) ~expected)))))
+      (for [[f cases] @core-tests
+            [args expected] (partition 2 cases)]
+        (if (:single-arg (meta f))
+            `(is-tc-e (~f ~args) ~expected)
+            `(is-tc-e (~f ~@args) ~expected)))))
 
 
 ;;; core vars test
 
 (add-test1 seq
-	[1 2 3] (t/NonEmptySeqable number)
-	[]      (t/Option (t/NonEmptyASeq t/Any)))
+    [1 2 3] (t/NonEmptySeqable number)
+    []      (t/Option (t/NonEmptyASeq t/Any)))
 
 (add-test1 first 
-	[8]           number
-	(seq [1 2 3]) number
-	nil           nil)
+    [8]           number
+    (seq [1 2 3]) number
+    nil           nil)
 
 (add-test1 rest
-	[1 2 3]       (core/ASeq number)
-	(seq [1 2 3]) (core/ASeq number)
-	[]            (core/ASeq t/Any))
+    [1 2 3]       (core/ASeq number)
+    (seq [1 2 3]) (core/ASeq number)
+    []            (core/ASeq t/Any))
 
 (add-test1 last
-	[1 2 3]       number
-	(seq [1 2 3]) number
-	[]            (t/Option t/Any))
+    [1 2 3]       number
+    (seq [1 2 3]) number
+    []            (t/Option t/Any))
 
 (add-test1 butlast
-	[1 2 3] (core/ASeq number)
-	(seq [1 2 3]) (core/ASeq number)
-	[]  (core/ASeq t/Any))
+    [1 2 3] (core/ASeq number)
+    (seq [1 2 3]) (core/ASeq number)
+    []  (core/ASeq t/Any))
 
 (add-test1 second
-	[1 2 3] number
-	[]      nil
-	nil     nil
-	(seq [1 2 3]) (t/Option number))
+    [1 2 3] number
+    []      nil
+    nil     nil
+    (seq [1 2 3]) (t/Option number))
 
 
 (add-test1 clj->js
-	{:a 1} t/Any)
+    {:a 1} t/Any)
 
 (add-test1 nil?
-	nil   boolean
-	"nil" boolean)
+    nil   boolean
+    "nil" boolean)
 
 (add-test1 ifn?
-	(fn [x] x) boolean
-	"foo"      boolean)
+    (fn [x] x) boolean
+    "foo"      boolean)
 
 (add-test1 identity
-	8       number
-	"hello" string
-	[0]     (t/Vec number))
+    8       number
+    "hello" string
+    [0]     (t/Vec number))
 
 (add-test take
-	[2 [1 2 3 4]] (core/ASeq int))
+    [2 [1 2 3 4]] (core/ASeq int))
 
 (add-test drop
-	[2 [1 2 3 4]] (core/ASeq int))
+    [2 [1 2 3 4]] (core/ASeq int))
 
 (add-test1 number?
-	8     boolean
-	[1 2] boolean)
+    8     boolean
+    [1 2] boolean)
 
 (add-test1 string?
-	"hello" boolean)
+    "hello" boolean)
 
 (add-test1 seq?
-	(seq [1 2 3]) boolean
-	[1 2 3]       boolean)
+    (seq [1 2 3]) boolean
+    [1 2 3]       boolean)
 
 (add-test apply
-	[+ [2 3]]               number
-	[str ["hello" "world"]] string)
+    [+ [2 3]]               number
+    [str ["hello" "world"]] string)
 
 (add-test conj
-	[["foo"] "bar"] (core/IVector string)
-	[(seq [3 4 5]) 1 2] (core/ASeq number)
-	[(seq [[1] [2 3]]) [8] [9]] (core/ASeq (t/Vec number))
-	;[{:foo 5} [:bar 8]] (t/Map core/Keyword number)
-	;[{:foo "bar"} {:baz 1 2 3}] (t/Map core/Keyword t/Any)
-	)
+    [["foo"] "bar"] (core/IVector string)
+    [(seq [3 4 5]) 1 2] (core/ASeq number)
+    [(seq [[1] [2 3]]) [8] [9]] (core/ASeq (t/Vec number))
+    ;[{:foo 5} [:bar 8]] (t/Map core/Keyword number)
+    ;[{:foo "bar"} {:baz 1 2 3}] (t/Map core/Keyword t/Any)
+    )
 
 (add-test get
-	[#{1 2 3} 3] (t/Option number)
-	[{:a true :b false} :c false] boolean)
+    [#{1 2 3} 3] (t/Option number)
+    [{:a true :b false} :c false] boolean)
 
 (add-test assoc
-	[["foo" "bar"] 2 "baz"] (t/Vec string)
-	[{[2 3] "foo"} [4 5] "bar"] (t/Map (t/Vec number) string))
+    [["foo" "bar"] 2 "baz"] (t/Vec string)
+    [{[2 3] "foo"} [4 5] "bar"] (t/Map (t/Vec number) string))
 
 (add-test dissoc
-	[{:foo 8 :bar 9} :foo] (t/Map core/Keyword number))
+    [{:foo 8 :bar 9} :foo] (t/Map core/Keyword number))
 
 (add-test1 fn?
-	(fn [x y] y)  boolean
-	cljs.core/map boolean)
+    (fn [x y] y)  boolean
+    cljs.core/map boolean)
 
 (add-test1 peek
-	[{:foo "bar" :baz "zot"} {:foo "bar"}] (t/Map core/Keyword string))
+    [{:foo "bar" :baz "zot"} {:foo "bar"}] (t/Map core/Keyword string))
 
 (add-test1 pop
-	[1 2 3] (t/Vec number))
+    [1 2 3] (t/Vec number))
 
 (add-test disj
-	[#{1 2 3 4} 3 4] (t/Set number))
+    [#{1 2 3 4} 3 4] (t/Set number))
+
+(add-test1 empty?
+    []  boolean
+    #{} boolean
+    [1] boolean
+    {:a 1} boolean)
+
+(add-test1 coll?
+    {:a 1} boolean
+    #{1 2} boolean
+    [2 3]  boolean
+    "foo"  boolean
+    2      boolean)
+
+(add-test1 map?
+    {:a 1} boolean
+    {}     boolean
+    [0 1]  boolean)
+
+(add-test1 vector?
+    []     boolean
+    [1]    boolean
+    {:a 1} boolean
+    "foo"  boolean)
+
+(add-test1 false?
+    []    boolean
+    false boolean
+    true  boolean
+    1     boolean)
+
+(add-test1 true?
+    []    boolean
+    false boolean
+    true  boolean
+    1     boolean)
+
+(add-test1 seq?
+    []           boolean
+    #{2 3}       boolean
+    (seq [1 2])  boolean
+    (seq #{2 3}) boolean)
+
+(add-test1 boolean
+    []     boolean
+    "true" boolean
+    :false boolean)
+
+(add-test1 integer?
+    2   boolean
+    1.3 boolean
+    "8" boolean)
+
+(add-test contains?
+    [[1 2 3] 2]    boolean
+    [#{1 2} 8]     boolean
+    [(seq []) "a"] boolean)
+
+(add-test find
+    [{:a 1 :b 2} :b] (t/Option (HVec [Keyword number]))
+    [[:a :b] 0]      (t/Option (HVec [int Keyword])))
+
+(add-test distinct?
+    [:a :b]       boolean
+    [[1] [1] [1]] boolean
+    [:a "foo" []] boolean)
+
+(add-test compare
+    [[1 2] [2 3]] number
+    ["foo" "bar"] number
+    [2 1]         number)
+
+(add-test sort
+    [[2 4 2 1 5 3]] (t/Option (core/ASeq int))
+    [[:a :c :b]]    (t/Option (core/ASeq Keyword))
+    [(t/fn [x :- int y :- int] :- int (- x y))
+     [6 1 7 3 2]]   (t/Option (core/ASeq int)))
+
+(add-test1 shuffle
+    [1 2 3 4] (t/Vec int)
+    #{4 8 2}  (t/Vec int))
+
+(add-test reduce
+    [(t/fn [x :- int y :- string] :- int 0)
+     ["foo" "bar" "baz"]] int
+    [(t/fn [x :- int y :- int] :- (core/Reduced int) (reduced 0))
+     #{8 36 2}] int
+    [(t/fn [x :- int y :- int] :- (core/Reduced int) (reduced 0))
+     #{8 36 2} 0] int)
+
 
 
 
@@ -139,4 +229,4 @@
 
 
 (deftest cljs-core-fns-refined-test
-	(run-core-tests))
+    (run-core-tests))
