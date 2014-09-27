15e00db24c343231a0be8274b163647c3f9c2500
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2014-09-27 09:58:14 +0800

    Add annotations for coerce-utils

--- module-check/src/main/clojure/clojure/core/typed/coerce_ann.clj
+++ module-check/src/main/clojure/clojure/core/typed/coerce_ann.clj
@@ -1,3 +1,6 @@
-(ns clojure.core.typed.coerce-ann
+(ns ^:skip-wiki clojure.core.typed.coerce-ann
   (:require [clojure.core.typed :as t]
             clojure.core.typed.coerce-utils))
+
+(t/ann ^:no-check clojure.core.typed.coerce-utils/Class->symbol [Class -> t/Sym])
+(t/ann ^:no-check clojure.core.typed.coerce-utils/symbol->Class [t/Sym -> Class])
