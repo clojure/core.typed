9940dc387d1a311dc9c97044291587f645906493
Aphyr <aphyr@aphyr.com>
2014-12-09 13:25:31 -0800

    check.monitor: Fix preconditions and require style
    Signed-off-by: Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>

--- module-check/src/main/clojure/clojure/core/typed/check/monitor.clj
+++ module-check/src/main/clojure/clojure/core/typed/check/monitor.clj
@@ -1,22 +1,20 @@
 (ns clojure.core.typed.check.monitor
-  (:require [clojure.core.typed [utils :as u]
-                                [type-rep :as r]
-                                [type-ctors :as c]
-                                [subtype :as sub]]
+  (:require [clojure.core.typed.utils :as u]
+            [clojure.core.typed.type-rep :as r]
+            [clojure.core.typed.type-ctors :as c]
+            [clojure.core.typed.subtype :as sub]
             [clojure.core.typed.check.utils :as cu]))
 
 (defn check-monitor
   "monitor-enter and monitor-exit both take any object and return nil"
   [check {:keys [target] :as expr} expected]
+  {:pre [(or (nil? expected) (r/TCResult? expected))]}
   ; We're going to return nil
   (let [t (r/ret r/-nil)]
     ; We need to verify that returning nil is OK; e.g. we expect nil.
     (when (r/TCResult? expected)
       (when-not (sub/subtype? t (r/ret-t expected))
         (cu/expected-error t (r/ret-t expected))))
-
-    ; If expected isn't a TCResult, it should be nil.
-    (assert (nil? expected))
 
     (assoc expr
            :target (check target (r/ret (c/RClass-of Object)))
