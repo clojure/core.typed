b751157124992502b0e964351c013069bc907171
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2015-02-21 00:34:58 +0000

    propositions for isa? are more conservative

--- src/main/clojure/clojure/core/typed/tc_equiv.clj
+++ src/main/clojure/clojure/core/typed/tc_equiv.clj
@@ -3,7 +3,15 @@
             [clojure.core.typed.type-ctors :as c]
             [clojure.core.typed.filter-rep :as fl]
             [clojure.core.typed.filter-ops :as fo]
+            [clojure.core.typed.contract-utils :as con]
             [clojure.math.combinatorics :as comb]))
+
+(defn equivable? [t]
+  {:pre [(r/Type? t)]
+   :post [(con/boolean? %)]}
+  (boolean
+    (when (r/Value? t)
+      ((some-fn number? symbol? keyword? nil? true? false? class?) (:val t)))))
 
 ;[Any TCResult * -> TCResult]
 (defn tc-equiv [comparator & vs]
@@ -13,19 +21,15 @@
   (assert (seq vs))
   (let [; TODO sequence behaviour is subtle
         ; conservative for now
-        equiv-able (fn [t]
-                     (boolean
-                       (when (r/Value? t)
-                         ((some-fn number? symbol? keyword? nil? true? false? class?) (:val t)))))
         vs-combinations (comb/combinations vs 2)
         ;_ (prn vs-combinations)
         then-filter (apply fo/-and (apply concat
                                           (for [[{t1 :t fl1 :fl o1 :o}
                                                  {t2 :t fl2 :fl o2 :o}] vs-combinations]
                                             (concat
-                                              (when (equiv-able t1) 
+                                              (when (equivable? t1) 
                                                 [(fo/-filter-at t1 o2)])
-                                              (when (equiv-able t2) 
+                                              (when (equivable? t2) 
                                                 [(fo/-filter-at t2 o1)])))))
         ;_ (prn then-filter)
         else-filter (apply fo/-or 
@@ -33,9 +37,9 @@
                                                    (for [[{t1 :t fl1 :fl o1 :o}
                                                           {t2 :t fl2 :fl o2 :o}] vs-combinations]
                                                      (concat
-                                                       (when (equiv-able t1) 
+                                                       (when (equivable? t1) 
                                                          [(fo/-not-filter-at t1 o2)])
-                                                       (when (equiv-able t2) 
+                                                       (when (equivable? t2) 
                                                          [(fo/-not-filter-at t2 o1)])))))]
                              fs
                              ; ensure we don't simplify to ff if we have more than one
