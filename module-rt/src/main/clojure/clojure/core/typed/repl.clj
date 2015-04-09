ffc57b88208e3bd1d16e2f0c7c99c37971b68da0
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2015-04-09 17:19:11 -0400

    parse ns forms

--- module-rt/src/main/clojure/clojure/core/typed/repl.clj
+++ module-rt/src/main/clojure/clojure/core/typed/repl.clj
@@ -8,17 +8,31 @@
 
 (defn wrap-clj-repl [handler]
   (fn [{:keys [code transport] :as msg}]
-    (let [typed? 
+    (let [current-ns (some-> msg :session deref (get #'*ns*))
+          _ (assert (instance? clojure.lang.Namespace current-ns))
+          typed? 
           (boolean
-            (some-> msg :session deref (get #'*ns*)
-                    meta :core.typed))]
+            (some-> current-ns meta :core.typed))
+          _ (prn code)
+          rcode (try (read-string code)
+                     (catch Throwable e))
+          ns-form? (and (coll? rcode) 
+                        (= (first rcode) 'ns)
+                        (= #'ns (ns-resolve current-ns 'ns))
+                        (-> (second rcode)
+                            meta :core.typed boolean))]
       (spit "Foo" (prn-str msg) :append true)
-      (when typed?
-        (let [{:keys [delayed-errors]} (t/check-form-info (read-string code))]
-          (when (seq delayed-errors)
-            (try (err/print-errors! delayed-errors)
-                 (catch Exception e
-                   (prn (.getMessage e)))))))
+      (prn "ns-form?" ns-form?)
+      (when (or ns-form? typed?)
+        (try
+          (let [{:keys [delayed-errors]} (t/check-form-info (read-string code))]
+            (when (seq delayed-errors)
+              (try (err/print-errors! delayed-errors)
+                   (catch Exception e
+                     (prn (.getMessage e))))))
+          (catch Throwable e
+            (prn "[core.typed] Unhandled exception while type checking\n\n"
+                 (.getMessage e)))))
       (handler msg))))
 
 (mid/set-descriptor! #'wrap-clj-repl
