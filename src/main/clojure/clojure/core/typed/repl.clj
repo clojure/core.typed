685fd81b9e6e3f05bf67116dc56bd44113c76e88
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2015-04-09 19:22:55 -0400

    evaluate code only after type checking

--- src/main/clojure/clojure/core/typed/repl.clj
+++ src/main/clojure/clojure/core/typed/repl.clj
@@ -1,14 +1,18 @@
 (ns clojure.core.typed.repl
   (:require [clojure.tools.nrepl.middleware :as mid]
-            [clojure.tools.nrepl.transport :as trans]
+            [clojure.tools.nrepl.transport :as transport]
             [clojure.tools.nrepl.misc :as misc]
             [clojure.core.typed :as t]
             [clojure.core.typed.errors :as err]
-            [clojure.tools.namespace.parse :as ns]))
+            [clojure.tools.namespace.parse :as ns])
+  (:import java.io.Writer))
 
 (defn wrap-clj-repl [handler]
-  (fn [{:keys [code transport] :as msg}]
-    (let [current-ns (some-> msg :session deref (get #'*ns*))
+  (fn [{:keys [code transport session op] :as msg}]
+    (let [flush (fn []
+                  (.flush ^Writer (@session #'*out*))
+                  (.flush ^Writer (@session #'*err*)))
+          current-ns (some-> session deref (get #'*ns*))
           _ (assert (instance? clojure.lang.Namespace current-ns))
           typed? 
           (boolean
@@ -20,20 +24,31 @@
                         (= (first rcode) 'ns)
                         (= #'ns (ns-resolve current-ns 'ns))
                         (-> (second rcode)
-                            meta :core.typed boolean))]
+                            meta :core.typed boolean))
+          should-check? (and (= "eval" op)
+                             (or typed? ns-form?))]
       (spit "Foo" (prn-str msg) :append true)
       (prn "ns-form?" ns-form?)
-      (when (or ns-form? typed?)
-        (try
-          (let [{:keys [delayed-errors]} (t/check-form-info (read-string code))]
-            (when (seq delayed-errors)
-              (try (err/print-errors! delayed-errors)
-                   (catch Exception e
-                     (prn (.getMessage e))))))
-          (catch Throwable e
-            (prn "[core.typed] Unhandled exception while type checking\n\n"
-                 (.getMessage e)))))
-      (handler msg))))
+      (if should-check?
+        (binding [*out* (@session #'*out*)]
+          (try
+            (let [{:keys [delayed-errors result]} (t/check-form-info (read-string code))]
+              (when (seq delayed-errors)
+                (try (err/print-errors! delayed-errors)
+                     (catch Exception e
+                       (prn (.getMessage e)))))
+              (flush)
+              (prn "Value =" result)
+              (transport/send transport (misc/response-for msg {:value (pr-str result)
+                                                                :printed-value true
+                                                                :ns (some-> session deref (get #'*ns*))})))
+            (catch Throwable e
+              (prn "[core.typed] Unhandled exception while type checking\n\n"
+                   (.getMessage e))
+              (flush)
+              )))
+        (handler msg))
+      (prn "FINISHED"))))
 
 (mid/set-descriptor! #'wrap-clj-repl
   {:requires #{"clone"}
