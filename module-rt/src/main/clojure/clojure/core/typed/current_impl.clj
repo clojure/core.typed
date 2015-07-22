856abf7d11e86eb660dfe25682be93285003d15c
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2015-07-23 01:47:36 +0800

    CTYP-256 - Extend impl-case with :unknown implementation
    The :unknown implementation is the default if no implementation is
    specified. If no :unknown case is specified in impl-case and the
    impl-case falls through, an exception is thrown as before.
    
    Also add impl-case tests.

--- module-rt/src/main/clojure/clojure/core/typed/current_impl.clj
+++ module-rt/src/main/clojure/clojure/core/typed/current_impl.clj
@@ -37,11 +37,11 @@
 (derive clojure any-impl)
 (derive clojurescript any-impl)
 
-(defonce ^:dynamic *current-impl* nil)
-(set-validator! #'*current-impl* (some-fn nil? keyword?))
+(defonce ^:dynamic *current-impl* :unknown)
+(set-validator! #'*current-impl* keyword?)
 
 (defmacro with-impl [impl & body]
-  `(do (assert ((some-fn #{~impl} nil?) *current-impl*) 
+  `(do (assert ((set [~impl :unknown]) *current-impl*)
                (str "Cannot overlay different core.typed implementations: " (pr-str *current-impl*)
                     ", expected " (pr-str ~impl)))
      (binding [*current-impl* ~impl]
@@ -114,7 +114,7 @@
 
 
 (defn implementation-specified? []
-  (boolean *current-impl*))
+  ((complement #{:unknown}) *current-impl*))
 
 (defn ensure-impl-specified []
   (assert (implementation-specified?) "No implementation specified"))
@@ -140,13 +140,15 @@
 (defn assert-cljs []
   (assert (= clojurescript *current-impl*) "Clojurescript implementation only"))
 
-(defmacro impl-case [& {clj-case :clojure cljs-case :cljs :as opts}]
-  (assert (= #{:clojure :cljs} (set (keys opts)))
+(defmacro impl-case [& {clj-case :clojure cljs-case :cljs unknown :unknown :as opts}]
+  (assert (empty? (set/difference (set (keys opts)) #{:clojure :cljs :unknown}))
           "Incorrect cases to impl-case")
-  `(case (current-impl)
+  `(case *current-impl*
      ~clojure ~clj-case
      ~clojurescript ~cljs-case
-     (assert nil (str "No case matched for impl-case" (current-impl)))))
+     ~(if (contains? opts :unknown)
+        unknown
+        `(assert nil (str "No case matched for impl-case " *current-impl*)))))
 
 (defn var->symbol [^clojure.lang.Var var]
   {:pre [(var? var)]
