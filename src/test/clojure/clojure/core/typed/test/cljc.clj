2862c36af8ef2b6608f7afd22524dffa7622d671
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2016-01-29 10:30:29 -0500

    CTYP-285: Bump tools.namespace 0.2.5 -> 0.2.11 for CLJC support

--- src/test/clojure/clojure/core/typed/test/cljc.clj
+++ src/test/clojure/clojure/core/typed/test/cljc.clj
@@ -1,9 +1,15 @@
 (ns clojure.core.typed.test.cljc
   (:require [clojure.test :refer :all]
-            [clojure.core.typed :as t]))
+            [clojure.core.typed :as t]
+            [clojure.core.typed.load :as load]))
 
 (deftest check-ns-should-load-cljc-files
   (is (t/check-ns 'clojure.core.typed.test.dummy-cljc :collect-only true)))
 
 (deftest check-ns-should-load-clj-files-before-cljc
   (is (t/check-ns 'clojure.core.typed.test.duplicated :collect-only true)))
+
+(deftest CTYP-285-ns-form-in-reader-cond-should-work
+  (is (thrown?
+        clojure.lang.ExceptionInfo
+        (load/typed-load1 "clojure/core/typed/test/ns_reader_cond"))))
---
+++ src/test/resources/clojure/core/typed/test/ns_reader_cond.cljc
@@ -0,0 +1,5 @@
+#?(:clj
+    (ns clojure.core.typed.test.ns-reader-cond
+      {:lang :core.typed}))
+
+#(inc 'a)
