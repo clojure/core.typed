69779ea7d06ca778e07720098fde10b26ba883d6
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2016-01-31 19:55:34 -0500

    CTYP-308: enable sanity-checking compilation for core.typed-rt project

--- src/main/clojure/clojure/core/typed/all_envs.clj
+++ src/main/clojure/clojure/core/typed/all_envs.clj
@@ -1,26 +1,27 @@
 (ns ^:skip-wiki clojure.core.typed.all-envs
-  (:require [clojure.core.typed.name-env :as nme-env]
-            [clojure.core.typed.parse-unparse :as prs]
-            [clojure.core.typed.util-vars :as vs]
-            [clojure.core.typed.var-env :as var-env]
+  (:require [clojure.core.typed.util-vars :as vs]
             [clojure.core.typed.current-impl :as impl]
-            ))
+            [clojure.core.typed.load-if-needed :refer [load-if-needed]]))
 
 (defn name-env []
-  (let [nme-env (nme-env/name-env)]
+  (load-if-needed)
+  (let [nme-env ((impl/v 'clojure.core.typed.name-env/name-env))]
     (binding [vs/*verbose-types* true]
       (into {}
             (for [[k v] nme-env]
               (when-not (keyword? v)
-                [k (prs/unparse-type v)]))))))
+                [k ((impl/v 'clojure.core.typed.parse-unparse/unparse-type)
+                    v)]))))))
 
 (defn var-env []
-  (let [var-env (var-env/var-annotations)]
+  (load-if-needed)
+  (let [var-env ((impl/v 'clojure.core.typed.var-env/var-annotations))]
     (assert var-env)
     (binding [vs/*verbose-types* true]
       (into {}
             (for [[k v] var-env]
-              [k (prs/unparse-type (force v))])))))
+              [k ((impl/v 'clojure.core.typed.parse-unparse/unparse-type)
+                  (force v))])))))
 
 (defn all-envs-clj []
   (impl/with-clojure-impl
