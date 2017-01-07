707d4adf03c1ec5493c075b4ee6a6545e7a25725
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2017-01-07 15:47:07 -0500

    Bump Clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.9.0-alpha11"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  ;[org.clojure/clojurescript "1.9.227"]
                  [com.taoensso/timbre "2.1.2"]
                  [org.clojure/core.match "0.2.0-alpha12"]
