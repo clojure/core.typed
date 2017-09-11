8e75e4361698e59364e83aa094f075083a9b532d
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2017-09-11 16:19:17 -0400

    port CLJ-1714 changes

--- module-check/src/main/java/clojure/core/typed/lang/Compiler.java
+++ module-check/src/main/java/clojure/core/typed/lang/Compiler.java
@@ -1079,7 +1079,7 @@ static public abstract class HostExpr implements Expr, MaybePrimitiveExpr{
 				if(Util.equals(sym,COMPILE_STUB_SYM.get()))
 					return (Class) COMPILE_STUB_CLASS.get();
 				if(sym.getName().indexOf('.') > 0 || sym.getName().charAt(0) == '[')
-					c = RT.classForName(sym.getName());
+					c = RT.classForNameNonLoading(sym.getName());
 				else
 					{
 					Object o = currentNS().getMapping(sym);
@@ -1090,7 +1090,7 @@ static public abstract class HostExpr implements Expr, MaybePrimitiveExpr{
 					else
 						{
 						try{
-						c = RT.classForName(sym.getName());
+						c = RT.classForNameNonLoading(sym.getName());
 						}
 						catch(Exception e){
 							// aargh
@@ -1101,7 +1101,7 @@ static public abstract class HostExpr implements Expr, MaybePrimitiveExpr{
 				}
 			}
 		else if(stringOk && form instanceof String)
-			c = RT.classForName((String) form);
+			c = RT.classForNameNonLoading((String) form);
 		return c;
 	}
 
