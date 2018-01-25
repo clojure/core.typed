3ccd1214d36ba1d7516e8a484824daa95d41fe81
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2018-01-25 17:43:20 -0500

    bench

--- module-check/src/test/clojure/clojure/core/typed/test/runtime_infer/bench_track.clj
+++ module-check/src/test/clojure/clojure/core/typed/test/runtime_infer/bench_track.clj
@@ -13,6 +13,11 @@
     nil
     (stack-depth (dec i))))
 
+(defn no-rewrap-f [i f]
+  (if (zero? i)
+    f
+    (no-rewrap-f (dec i) f)))
+
 (defn traverse-map-once [i m]
   (if (zero? i)
     (:a m)
@@ -24,6 +29,35 @@
     (call-if-one (dec i) f)))
 
 (comment
+(defmacro bench
+  "Evaluates expr and returns the time it took."
+  [expr]
+  `(let [start# (. System (nanoTime))
+         ret# ~expr
+         msduration# (/ (double (- (. System (nanoTime)) start#)) 1000000.0)]
+     [ret# msduration#]))
+
+(defn bench-iteratively 
+  ([f n] (bench-iteratively f 0 n))
+  ([f start n]
+   (loop [times []
+          i start]
+     (if (< n i)
+       times
+       (let [[_ t] (f i)]
+         (recur (conj times t)
+                (inc i)))))))
+
+(defn write-csv [n v]
+  {:pre [(vector? v)]}
+  (spit n (apply str (interpose "," v))))
+
+  ;; tracking overhead?
+(write-csv
+  "no-track-stack-depth.csv"
+  (bench-iteratively
+    (bench (stack-depth i))))
+
   (stack-depth 1000)
   (traverse-map-once 
     3000
