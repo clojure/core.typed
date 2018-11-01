b6703e0bbcd70bcaa488d435c2da1a03a9fbf347
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2018-11-01 17:46:40 -0400

    remove old collect code

--- src/main/clojure/clojure/core/typed/check_ns_cljs.clj
+++ src/main/clojure/clojure/core/typed/check_ns_cljs.clj
@@ -2,15 +2,8 @@
   (:require [cljs.compiler :as comp]
             [clojure.core.typed.current-impl :as impl]
             [cljs.env :as env]
-            [cljs.compiler :as comp]
-            [clojure.core.typed.util-vars :as vs]
             [clojure.core.typed.util-cljs :as ucljs]
-            [clojure.core.typed.reset-caches :as reset-caches]
-            [clojure.core.typed.reset-env :as reset-env]
-            [clojure.core.typed.collect-cljs :as clt-cljs]
-            [clojure.core.typed.check-cljs :as chk-cljs]
-            [clojure.core.typed.check-ns-common :as chk-ns]
-            [clojure.core.typed.errors :as err]))
+            [clojure.core.typed.check-ns-common :as chk-ns]))
 
 (defn check-ns-info
   [ns-or-syms opt]
