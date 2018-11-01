84ced8e09331ebebe767fdf6a94ac945c05028a5
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2018-11-01 18:01:15 -0400

    dead code

--- module-check/src/main/clojure/clojure/core/typed/collect/gen_datatype.clj
+++
@@ -1,8 +0,0 @@
-(ns clojure.core.typed.collect.gen-datatype
-  (:require [clojure.core.typed.current-impl :as impl]
-            [clojure.core.typed.parse-unparse :as prs]))
-
-(defn parse-field [[n _ t]] 
-  [n (prs/parse-type t)])
-
-(def gen-datatype* impl/gen-datatype*)
--- module-check/src/main/clojure/clojure/core/typed/collect/gen_protocol.clj
+++
@@ -1,4 +0,0 @@
-(ns clojure.core.typed.collect.gen-protocol
-  (:require [clojure.core.typed.current-impl :as impl]))
-
-(def gen-protocol* impl/gen-protocol*)
