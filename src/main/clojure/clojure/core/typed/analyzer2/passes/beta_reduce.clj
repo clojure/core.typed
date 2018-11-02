3c4ed26fb876e089eb5c3bb372ad7dba37e88baf
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2018-11-02 13:23:06 -0400

    add copyright notices

--- src/main/clojure/clojure/core/typed/analyzer2/passes/beta_reduce.clj
+++ src/main/clojure/clojure/core/typed/analyzer2/passes/beta_reduce.clj
@@ -1,3 +1,11 @@
+;;   Copyright (c) Ambrose Bonnaire-Sergeant, Rich Hickey & contributors.
+;;   The use and distribution terms for this software are covered by the
+;;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;;   which can be found in the file epl-v10.html at the root of this distribution.
+;;   By using this software in any fashion, you are agreeing to be bound by
+;;   the terms of this license.
+;;   You must not remove this notice, or any other, from this software.
+
 ;; should be a JVM pass since it calls `run-passes`
 (ns clojure.core.typed.analyzer2.passes.beta-reduce
   (:require [clojure.core.typed.analyzer2.passes.add-binding-atom :as add-binding-atom]
