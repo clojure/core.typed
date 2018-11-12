e5ae0d97b3721aed1c87f9440c8b902205eb5e9e
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2018-11-11 19:29:10 -0500

    move statistics

--- src/main/clojure/clojure/core/typed/checker/jvm/statistics.clj
+++ src/main/clojure/clojure/core/typed/checker/jvm/statistics.clj
@@ -6,7 +6,7 @@
 ;;   the terms of this license.
 ;;   You must not remove this notice, or any other, from this software.
 
-(ns clojure.core.typed.statistics
+(ns clojure.core.typed.checker.jvm.statistics
   (:require [clojure.core.typed.var-env :as var-env]
             [clojure.core.typed.checker.jvm.check-ns-clj :as chk-ns-clj]
             [clojure.core.typed.contract-utils :as con]
