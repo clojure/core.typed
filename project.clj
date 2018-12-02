55a3868c3213a65ae8c943e7660bc787dce504f4
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2018-12-01 22:59:16 -0500

    move to lein

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject org.typedclojure/core.typed.lang.jvm "0.7.0-SNAPSHOT"
+  :description "Extensible languages in Clojure, a la Racket's #lang."
+  :url "https://github.com/typedclojure/core.typed.lang.jvm"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]]
+  :source-paths ["src/main/clojure/"]
+  :test-paths ["src/test/clojure/"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/tools.namespace "0.3.0-alpha4"]])
