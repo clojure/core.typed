424be76459aebeeab7e05e0ab5693885879b5662
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2018-12-01 23:58:27 -0500

    sign

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/typedclojure/core.typed.lang.jvm"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :deploy-repositories [["releases"  {:sign-releases false :url "https://clojars.org/repo"}]
-                        ["snapshots" {:sign-releases false :url "https://clojars.org/repo"}]]
+  :deploy-repositories [["releases"  {#_#_:sign-releases false :url "https://clojars.org/repo"}]
+                        ["snapshots" {#_#_:sign-releases false :url "https://clojars.org/repo"}]]
   :source-paths ["src/main/clojure/"]
   :test-paths ["src/test/clojure/"]
   :dependencies [[org.clojure/clojure "1.9.0"]
