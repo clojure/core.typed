d9ece878f8c7ca89ed3f3bd58f5515ccee9b1703
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2018-12-03 14:38:47 -0500

    move lib.clojure

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/typedclojure/core.typed.lib.clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {"sonatype-oss-public" {:url "https://oss.sonatype.org/content/groups/public/"}}
   :deploy-repositories [["releases"  {:sign-releases false :url "https://clojars.org/repo"}]
                         ["snapshots" {:sign-releases false :url "https://clojars.org/repo"}]]
   :release-tasks [["vcs" "assert-committed"]
@@ -15,4 +16,5 @@
                   ["vcs" "push"]]
   :source-paths ["src/main/clojure/"]
   :test-paths ["src/test/clojure/"]
-  :dependencies [[org.clojure/clojure "1.9.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.typed.runtime.jvm "0.7.1-SNAPSHOT"]])
