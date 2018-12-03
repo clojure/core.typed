5fd81305ea0b1e2074c1e81e485d08dffa52f391
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2018-12-02 19:55:30 -0500

    init

---
+++ project.clj
@@ -0,0 +1,18 @@
+(defproject org.typedclojure/core.typed.lib.clojure "0.7.0-SNAPSHOT"
+  :description "Type annotations and macros for the base Clojure distribution."
+  :url "https://github.com/typedclojure/core.typed.lib.clojure"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories [["releases"  {:sign-releases false :url "https://clojars.org/repo"}]
+                        ["snapshots" {:sign-releases false :url "https://clojars.org/repo"}]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "v" "--no-sign"]
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
+  :source-paths ["src/main/clojure/"]
+  :test-paths ["src/test/clojure/"]
+  :dependencies [[org.clojure/clojure "1.9.0"]])
