2fc2326653ea0613ef7f97139d58d775af6536bb
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2018-12-05 00:18:19 -0500

    sync-master.sh

---
+++ script/sync-master.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+branch_name="$(git symbolic-ref HEAD 2>/dev/null)" ||
+branch_name="(unnamed branch)"     # detached HEAD
+
+branch_name=${branch_name##refs/heads/}
+
+MASTER="master"
+
+set -e
+
+if [ $branch_name != "$MASTER" ]; then
+  echo "Must be on $MASTER"
+  exit 1;
+fi
+
+git pull clojure --ff-only master
+git pull typedclojure --ff-only master
+git push typedclojure master
+git push clojure master
