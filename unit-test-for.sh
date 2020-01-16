3b2248a08adc79987476f38dc3efbdb47ee5219f
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2020-01-16 00:14:21 -0500

    update script

--- unit-test-for.sh
+++ unit-test-for.sh
@@ -1,5 +1,5 @@
-#! /bin/sh
+#!/bin/sh
 
-touch module-check/src/test/clojure/clojure/core/typed/test/ctyp_$1.clj
+touch module-checker.jvm/src/test/clojure/clojure/core/typed/test/ctyp_$1.clj
 
 echo "(ns clojure.core.typed.test.ctyp-${1}\n  (:require [clojure.test :refer :all]\n            [clojure.core.typed.test.test-utils :refer :all]))" >> module-check/src/test/clojure/clojure/core/typed/test/ctyp_$1.clj
