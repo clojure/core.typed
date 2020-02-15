e172f3cebe9c65619238b2209d84ae717837b4e0
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2020-02-15 15:41:56 -0500

    add test paths to dev-repl and fix checker.jvm test script

--- dev/src/typed/dev/merge_deps.clj
+++ dev/src/typed/dev/merge_deps.clj
@@ -13,8 +13,16 @@
   '{:nREPL
     {:extra-deps
      {nrepl/nrepl {:mvn/version "0.6.0"}
+      cider/cider-nrepl {:mvn/version "0.22.4"}
       cider/piggieback {:mvn/version "0.3.8"}}
-     :main-opts ["-m" "nrepl.cmdline" "--interactive"]}})
+     :main-opts ["-m" "nrepl.cmdline" "--interactive"
+                 #_"
+                 Note:
+                   introducing other middleware makes vim-fireplace choose
+                   fipp for pprint, which doesn't play well with the delicately
+                   defined classes in type-rep."
+                 "--middleware" "[cider.nrepl/wrap-complete]"
+                 ]}})
 
 (defn -main [& args]
   (let [deps-maps (->> (File. everything-root relative-projects-root)
@@ -27,6 +35,8 @@
                                       slurp
                                       read-string)])))
                        (into {}))
+        expand-deps (juxt identity
+                          (comp :test :aliases))
         everything-deps {:deps (apply merge-with
                                       (fn [v1 v2]
                                         (if (= v1 v2)
@@ -34,14 +44,23 @@
                                           (throw (ex-info (str "Version conflict: "
                                                                v1 " " v2)
                                                           {:versions [v1 v2]}))))
-                                      (keep :deps (vals deps-maps)))
+                                      (->> (vals deps-maps)
+                                           (mapcat expand-deps)
+                                           (mapcat (fn [d]
+                                                     {:pre [(map? d)]}
+                                                     (concat (some-> d :deps vector)
+                                                             (some-> d :extra-deps vector))))))
                          :paths (vec (mapcat
-                                       (fn [[^String fname {:keys [paths]}]]
-                                         (map #(str (-> relative-projects-root
-                                                        (File. fname)
-                                                        str
-                                                        (File. ^String %)))
-                                              paths))
+                                       (fn [[^String fname d]]
+                                         (let [path->relative #(str (-> relative-projects-root
+                                                                        (File. fname)
+                                                                        str
+                                                                        (File. ^String %)))]
+                                           (mapcat (fn [d]
+                                                     (let [all-paths (concat (:paths d)
+                                                                             (:extra-paths d))]
+                                                       (map path->relative all-paths)))
+                                                   (expand-deps d))))
                                        deps-maps))
                          :aliases aliases}]
     (spit (str (File. everything-root "deps.edn"))
